27fc162614ac803392789a961edcf61a4743e37d
EmilyCh0 <seoyeon3089@naver.com>
2022-12-17 14:12:55 +0900

    feat(InputView): 메뉴 추천 받을 코치 이름 입력 받기

--- src/main/kotlin/menu/InputView.kt
+++ src/main/kotlin/menu/InputView.kt
@@ -3,8 +3,9 @@ package menu
 import camp.nextstep.edu.missionutils.Console
 
 class InputView {
-    fun readCoachNames() {
-
+    fun readCoachNames(): String {
+        println("코치의 이름을 입력해 주세요. (, 로 구분)")
+        return Console.readLine()
     }
 
     fun readDislikeMenus() {
