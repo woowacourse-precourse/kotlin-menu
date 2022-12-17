aa3faeeb3458a486a6ec1c1c0499e26940297b19
s9hn <tnghqn@naver.com>
2022-12-17 14:36:20 +0900

    [FEAT(NoticeMessage)] 안내문구 상수화

--- src/main/kotlin/menu/data/NoticeMessage.kt
+++ src/main/kotlin/menu/data/NoticeMessage.kt
@@ -1,3 +1,5 @@
 package menu.data
 
-const val PROGRAM_START = "점심 메뉴 추천을 시작합니다."
+const val PROGRAM_START = "점심 메뉴 추천을 시작합니다.\n"
+const val ENTER_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)"
+const val ENTER_NO_EATING = "(이)가 못 먹는 메뉴를 입력해 주세요."
