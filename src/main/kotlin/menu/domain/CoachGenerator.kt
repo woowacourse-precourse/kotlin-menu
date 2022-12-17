72882b40b828d4b158a0a0a23733adebe2ab1180
Gloria <ippnsj@gmail.com>
2022-12-17 16:47:53 +0900

    fix: 코치 클래스 필드가 추가됨에 따라 코치 메이커 클래스 필드 추가

--- src/main/kotlin/menu/domain/CoachGenerator.kt
+++ src/main/kotlin/menu/domain/CoachGenerator.kt
@@ -2,11 +2,11 @@ package menu.domain
 
 import menu.dto.CoachesDTO
 
-class CoachGenerator {
+class CoachGenerator(private val generator: MenuGenerator) {
     fun generate(inputs: Map<String, List<String>>): CoachesDTO {
         val coaches = mutableListOf<Coach>()
         inputs.map { (name, menus) ->
-            coaches.add(Coach(name, menus))
+            coaches.add(Coach(name, menus, generator))
         }
         return CoachesDTO(coaches)
     }
