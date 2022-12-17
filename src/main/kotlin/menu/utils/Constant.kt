adc10384657a9d260ca6a60ef3c92e628d45e56c
JunJangE <wnswkd486@gmail.com>
2022-12-17 14:01:42 +0900

    feat: COACH_NAME_INPUT_TYPE_ERROR_MESSAGE 추가

--- src/main/kotlin/menu/utils/Constant.kt
+++ src/main/kotlin/menu/utils/Constant.kt
@@ -1,8 +1,10 @@
 package menu.utils
 
 object Constant {
-    const val MENU_RECOMMENDATION_START_MESSAGE = "점심 메뉴 추천을 시작합니다."
-    const val COACH_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)"
+    const val MENU_RECOMMENDATION_START_MESSAGE = "점심 메뉴 추천을 시작합니다.\n"
+    const val COACH_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)\n"
+    const val COACH_UNEATABLE_MENU_MESSAGE = "%d(이) 가 못 먹는 메뉴를 입력해 주세요."
+
     const val ERROR_MESSAGE = "[ERROR] 잘못입력하셨습니다."
-    const val COACH_NAME_INPUT_TYPE_ERROR_MESSAGE = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자여야 합니다."
+    const val COACH_NAME_INPUT_TYPE_ERROR_MESSAGE = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자여야 합니다.\n"
 }
