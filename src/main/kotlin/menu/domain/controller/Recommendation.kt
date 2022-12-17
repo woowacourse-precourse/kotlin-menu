93b9bffbfddc778d06344ca0eca0b8c3b65302d7
wonseok <dnjstjr245@gmail.com>
2022-12-17 17:45:45 +0900

    [feat] 점심 메뉴 추천 시스템을 관리하는 클래스인  클래스 구현

---
+++ src/main/kotlin/menu/domain/controller/Recommendation.kt
@@ -0,0 +1,55 @@
+package menu.domain.controller
+
+import menu.data.State.categories
+import menu.data.State.coachNames
+import menu.data.State.inedibleFoods
+import menu.domain.CategoryMaker
+import menu.domain.CategoryRandomNumberGenerator
+import menu.domain.use_case.UseCases
+
+class Recommendation(
+    private val useCases: UseCases = UseCases(),
+    private val categoryMaker: CategoryMaker = CategoryMaker(CategoryRandomNumberGenerator())
+) : RecommendService {
+
+    override fun init() {
+        println(START_RECOMMENDATION)
+        categoryMaker.makeCategory()
+    }
+
+    override fun run() {
+        init()
+
+        // 로깅
+        println(categories)
+
+        processCoachName()
+        processInedibleFoods()
+        processRecommendation()
+
+        // 로깅
+        println(coachNames)
+        println(inedibleFoods)
+
+        end()
+    }
+
+    override fun end() {
+        println(END_RECOMMENDATION)
+    }
+
+    private fun processCoachName() =
+        useCases.validateCoachNameUseCase()
+
+    private fun processInedibleFoods() =
+        useCases.validateInedibleFoodUseCase()
+
+    private fun processRecommendation() =
+        useCases.recommendFoodsUseCase()
+
+
+    companion object {
+        const val START_RECOMMENDATION = "점심 메뉴 추천을 시작합니다.\n"
+        const val END_RECOMMENDATION = "추천을 완료했습니다."
+    }
+}
