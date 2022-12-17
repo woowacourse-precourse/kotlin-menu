f81a425ce234dd08848b7f6081ac337fe6a1da28
SeungHyeon Shim <tladhehd@naver.com>
2022-12-17 13:40:51 +0900

    feat: ErrorMessage 추가

---
+++ src/main/kotlin/menu/ErrorMessage.kt
@@ -0,0 +1,9 @@
+package menu
+
+class ErrorMessage {
+    private val Error = "[ERROR]"
+    val onlyLetter = Error+"글자만 입력해야 합니다."
+    val wrongLength = Error+"코치 이름길이는 2~4입니다."
+    val wrongNumber = Error+"코치는 2명 이상입니다"
+    val wrongMenuNum = Error+"못먹는 메뉴는 최대 2개입니다"
+}
