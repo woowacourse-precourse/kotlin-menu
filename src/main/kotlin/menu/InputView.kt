83c5213a510986227ec764608ef200265a3ecb05
healthykim <bsbs8645@snu.ac.kr>
2022-12-17 15:51:14 +0900

    feat(InputView): Add function for getting hate menu w/o error handling

--- src/main/kotlin/menu/InputView.kt
+++ src/main/kotlin/menu/InputView.kt
@@ -7,5 +7,10 @@ class InputView {
         println("코치의 이름을 입력해 주세요. (, 로 구분)")
         return Console.readLine().split(",")
     }
+
+    fun getHateMenu(name: String) :List<String> {
+        println("${name}(이)가 못 먹는 메뉴를 입력해 주세요.")
+        return Console.readLine().split(",")
+    }
 }
 
