1b03e2ad23604ff1843f204768d6e051fa734989
mendel <mendel.urssu@gmail.com>
2022-12-17 17:10:50 +0900

    fix: 싫어하는 음식에 빈 칸 입력 시 재입력 요청하던 버그 수정

--- src/main/kotlin/menu/InputValidator.kt
+++ src/main/kotlin/menu/InputValidator.kt
@@ -13,8 +13,8 @@ object InputValidator {
     }
 
     fun hateMenu(input: String, allMenus: List<List<String>>) {
+        if(input.trim().isEmpty()) return // 빈 것은 괜찮음. 못 먹는 음식이 없다는 의미.
         val hateMenus = input.split(",").map { it.trim() }
-        if (hateMenus.isEmpty()) return // 빈 것은 괜찮음. 못 먹는 음식이 없다는 의미.
         if (hateMenus.size > MAX_HATE_FOOD) throwError(Error.ERROR_HATE_MENU_MAX_TWO)
         if (hateMenus.size != hateMenus.distinct().size) throwError(Error.ERROR_HATE_MENU_DUPLICATE)
         val allMenuOne = mutableListOf<String>()
