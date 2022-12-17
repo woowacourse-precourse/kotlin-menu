19828a4d90c187b9df227d4c2c71d9326782e6e5
Boram <lboram20@naver.com>
2022-12-17 15:57:35 +0900

    feat<MenuRecommend>: 랜덤으로 선택된 카테고리가 다른 요일에 추천된 카테고리와 겹치는 지 확인한다

--- src/main/kotlin/menu/MenuRecommend.kt
+++ src/main/kotlin/menu/MenuRecommend.kt
@@ -3,6 +3,7 @@ package menu
 import camp.nextstep.edu.missionutils.Randoms
 
 class MenuRecommend {
+    private val categories = listOf("일식", "한식", "중식", "아시안", "양식")
     private val japaneseMenu = listOf("규동","우동","미소시루","스시","가츠동","오니기리","하이라이스","라멘","오코노미야끼")
     private val koreanMenu = listOf("김밥","김치찌개","쌈밥","된장찌개","비빔밥","칼국수","불고기","떡볶이","제육볶음")
     private val chineseMenu = listOf("깐풍기","볶음면","동파육","짜장면","짬뽕","마파두부","탕수육","토마토 달걀볶음","고추잡채")
@@ -10,11 +11,21 @@ class MenuRecommend {
     private val westernMenu = listOf("라자냐","그라탱","뇨끼","끼슈","프렌치 토스트","바게트","스파게티","피자","파니니")
 
     /** 1:일식, 2:한식, 3:중식, 4:아시안, 5:양식 **/
-    fun pickCategory() : String{
-        val categories = listOf("일식", "한식", "중식", "아시안", "양식")
-        val category: String = categories.get(Randoms.pickNumberInRange(1, 5))
+    fun pickCategory(recommendedCategory: List<String>): String {
+        while (true) {
+            val category: String = categories.get(Randoms.pickNumberInRange(1, 5))
+            val duplicate = isCategoryDuplicated(recommendedCategory, category)
+            if (!duplicate) { //겹치지않는다면 이제 메뉴를 고를 차례로 넘어간다
+                return category
+            }
+        }
+    }
 
-        return category
+    private fun isCategoryDuplicated(recommendedCategory: List<String>, category: String): Boolean {
+        if (recommendedCategory.contains(category)) {
+            return true
+        }
+        return false
     }
 
     fun getSpecificMenuList(menuCategory: String): List<String>{
