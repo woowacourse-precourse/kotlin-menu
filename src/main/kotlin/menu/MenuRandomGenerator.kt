a9b6d732b0cbea3206340c362d0fd2606264f94b
choiwoosuk <wooseokchoi0508@gmail.com>
2022-12-17 14:37:20 +0900

    feat: 추천할 메뉴 생성 기능 추가

---
+++ src/main/kotlin/menu/MenuRandomGenerator.kt
@@ -0,0 +1,9 @@
+package menu
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class MenuRandomGenerator:MenuGenerator {
+    override fun generate(menus: List<String>): String {
+        return Randoms.shuffle(menus)[0]
+    }
+}
