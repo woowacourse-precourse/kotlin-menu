32e91444cb512167728a2e72c51a3fd386978744
윤동현 <donghyunyoon81@gmail.com>
2022-12-17 14:28:31 +0900

    feat<InputView> : readCoachesName 기능 구현
    코치들의 이름을 입력 받는 기능 구현

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -1,4 +1,16 @@
 package menu.view
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
+
+    private val outputView = OutputView()
+    private val viewValidate = ViewValidate()
+
+    fun readCoachesName():List<String> {
+        outputView.printInputCoachNames()
+        val coachNames = Console.readLine()
+        viewValidate.coachNamesForm(coachNames)
+        return coachNames.split(",")
+    }
 }
