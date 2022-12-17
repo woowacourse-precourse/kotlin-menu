3064ac0d6cf5b6bac551916997b3183d4b28f7eb
bmsk <vv99911@gamil.com>
2022-12-17 17:26:20 +0900

    [REFACTOR] 모든 strings 적용

--- src/main/kotlin/menu/view/MenuView.kt
+++ src/main/kotlin/menu/view/MenuView.kt
@@ -3,6 +3,7 @@ package menu.view
 import camp.nextstep.edu.missionutils.Console
 import menu.exceptions.InvalidCoachCount
 import menu.exceptions.InvalidNameLengthException
+import menu.strings.GenericString
 import menu.strings.MenuErrorMessage
 import menu.strings.MenuNames
 import menu.view.actor.Calendar
@@ -16,9 +17,7 @@ class MenuView {
     private val calender = Calendar()
 
     fun start() {
-        println("점심 메뉴 추천을 시작합니다.") // 이거 string에
-        println()
-
+        println(GenericString.GUID_MENU_START.str + "\n")
         while (true) {
             try {
                 initSettings()
@@ -36,7 +35,7 @@ class MenuView {
     }
 
     private fun initSettings() {
-        println("코치의 이름을 입력해 주세요. (, 로 구분)")
+        println(GenericString.GUID_INPUT_COACH_NAME.str)
         val tokenizer = StringTokenizer(Console.readLine(), ",")
         val coachNames = tokenizer.toList()
         validateCoachCount(coachNames.size)
@@ -44,12 +43,11 @@ class MenuView {
         for (name in coachNames) {
             validateCoachNameLength(name.toString())
             coaches.add(Coach(name.toString()))
+            println("${name}${GenericString.GUID_INPUT_HATED_MENU.str}")
 
-            println("${name}(이)가 못 먹는 메뉴를 입력해 주세요.")
             val tokenizer2 = StringTokenizer(Console.readLine(), ",").toList()
-            for (item in tokenizer2) {
+            for (item in tokenizer2)
                 coaches.last().addDenialMenu(item.toString())
-            }
             println()
         }
     }
@@ -66,14 +64,14 @@ class MenuView {
     }
 
     private fun printResult() {
-        println("메뉴 추천 결과입니다.")
+        println(GenericString.GUID_MENU_RESULT.str)
         println(calender.toString())
         println(recommender.toStringCategoryRecord())
         for (coach in coaches) {
             println(coach.toStringSummary())
         }
         println()
-        println("추천을 완료했습니다.")
+        println(GenericString.GUID_COMPLETE.str)
     }
 
     private fun validateCoachCount(count: Int) {
