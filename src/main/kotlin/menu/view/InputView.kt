19806ac8676c25aee023244cef27a5dcd66fb14c
otter <a33397397@gmail.com>
2022-12-17 14:02:45 +0900

    feat(inputView): 입력값 검증 기능 적용

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -9,13 +9,13 @@ class InputView(
 
     fun readRecommendCoaches(): List<String> {
         val input = Console.readLine()
-
+        validator.validateRecommendCoaches(input)
         return input.split(",").toList()
     }
 
     fun readCoachCantEatMenus(): List<String> {
         val input = Console.readLine()
-
+        validator.validateCoachCantEatMenus(input)
         return input.split(",").toList()
     }
 }
