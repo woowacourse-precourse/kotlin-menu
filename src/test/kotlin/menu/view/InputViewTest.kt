b281fa0efd764a55a9b14142a6a5623f084c27c8
krrong <khk9664@naver.com>
2022-12-17 17:43:53 +0900

    test(InputView): 입력 테스트코드 작성

--- src/test/kotlin/menu/view/InputViewTest.kt
+++ src/test/kotlin/menu/view/InputViewTest.kt
@@ -13,13 +13,28 @@ import org.junit.jupiter.params.provider.ValueSource
 internal class InputViewTest : NsTest(){
     @ParameterizedTest
     @ValueSource(strings = ["q", "asdfasdf", "asdf,asdf,asdfasdf"])
-    fun `입력 테스트1`(input: String){
+    fun `예외입력 테스트1`(input: String){
         assertSimpleTest{
             runException(input)
             assertThat(output().contains(ErrorMessage.COACH_NUMBER))
         }
     }
 
+    @Test
+    fun `예외입력 테스트2`(){
+        assertSimpleTest{
+            runException("토미,제임스", "우동,스시,라멘,규동")
+            assertThat(output().contains(ErrorMessage.UNAVAILABLE_NUMBER))
+        }
+    }
+
+    @Test
+    fun `정상입력 테스트1`(){
+        assertSimpleTest{
+            runException("토미,제임스", "우동,스시", "뇨끼,월남쌈")
+            assertThat(output().contains(""))
+        }
+    }
 
     override fun runMain() {
         main()
