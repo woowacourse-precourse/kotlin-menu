abdcb3a1cba957686ecbe6ecd94300f427a55d76
pingu244 <69796976+pingu244@users.noreply.github.com>
2022-12-17 15:18:07 +0900

    feat: 입력 음식이 메뉴에 없는 예외 처리

--- src/main/kotlin/menu/Validator.kt
+++ src/main/kotlin/menu/Validator.kt
@@ -25,8 +25,25 @@ class Validator {
     }
 
     fun validateFood(food: List<String>) {
+        checkFoodSize(food)
+        for (cantEat in food)
+            checkFoodIsExistInMenu(cantEat)
+    }
+
+    private fun checkFoodSize(food: List<String>) {
         if (food.size > 2) {
-            OutputView().printErrorMessage(ERROR_FOOD_NUM)
+            OutputView().printErrorMessage(ERROR_FOOD_SIZE)
+            throw IllegalArgumentException()
+        }
+    }
+
+    private fun checkFoodIsExistInMenu(food: String) {
+        var check = false
+        for (menu in Food.values())
+            if (menu.menu.contains(food))
+                check = true
+        if (!check) {
+            OutputView().printErrorMessage(ERROR_FOOD_NOT_IN_MENU)
             throw IllegalArgumentException()
         }
     }
