d0deb6a3671ca122949e43f620390b1b6f660b9c
Joonhoo <mnc5@naver.com>
2022-12-17 17:27:11 +0900

    feat: 코치들의 최소인원과 쵀대인원에 대한 요구사항 예외확인 추가

--- src/main/kotlin/menu/util/Constants.kt
+++ src/main/kotlin/menu/util/Constants.kt
@@ -1,8 +1,11 @@
 package menu.util
 
+const val MIN_COACH_SIZE = 2
+const val MAX_COACH_SIZE = 5
 const val MIN_NAME_SIZE = 2
 const val MAX_NAME_SIZE = 4
 
 const val INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)"
 const val ERROR_HEADER = "[ERROR] "
 const val ERROR_COACH_NAME_LENGTH = "코치의 이름은 최소 2글자, 최대 4글자입니다."
+const val ERROR_COACH_SIZE = "코치는 최소 2명 이상 5명 이하로 입력해야 합니다."
--- src/main/kotlin/menu/util/Exceptions.kt
+++ src/main/kotlin/menu/util/Exceptions.kt
@@ -4,6 +4,7 @@ object Exceptions {
 
     // 코치들 입력값에 대한 (,) 구분 예외 확인
     fun checkCoachesInput(coaches: List<String>) {
+        checkCoachSize(coaches)
         coaches.forEach { coach ->
             checkCoachNameSize(coach)
         }
@@ -13,6 +14,13 @@ object Exceptions {
     private fun checkCoachNameSize(coach: String) {
         if (coach.length !in MIN_NAME_SIZE..MAX_NAME_SIZE) {
             throw IllegalArgumentException(ERROR_HEADER + ERROR_COACH_NAME_LENGTH)
+        }
+    }
+
+    // 코치들은 최소 2명에서 5명까지 같이 식사를 한다.
+    private fun checkCoachSize(coaches: List<String>) {
+        if (coaches.size !in MIN_COACH_SIZE..MAX_COACH_SIZE) {
+            throw IllegalArgumentException(ERROR_HEADER + ERROR_COACH_SIZE)
         }
     }
 }
