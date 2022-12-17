339f83023b5048b6aceed143acf70b2192ea7676
lee989898 <wnsrud1352@gmail.com>
2022-12-17 13:29:03 +0900

    [refactor](InputView) split 함수 delimiters 상수화

--- src/main/kotlin/menu/InputView.kt
+++ src/main/kotlin/menu/InputView.kt
@@ -5,12 +5,16 @@ import camp.nextstep.edu.missionutils.Console
 class InputView {
 
     fun readCoachesName(): List<String> {
-        val coachesName = Console.readLine().split(",")
+        val coachesName = Console.readLine().split(DELIMITERS)
         return coachesName
     }
 
     fun readCoachCantEatMenu(): List<String> {
-        val coachCantEat = Console.readLine().split(",")
+        val coachCantEat = Console.readLine().split(DELIMITERS)
         return coachCantEat
+    }
+
+    companion object {
+        private const val DELIMITERS = ","
     }
 }
