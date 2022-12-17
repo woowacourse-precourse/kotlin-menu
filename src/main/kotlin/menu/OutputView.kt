74edf672901bc011fb45fd26aaf0c563089c32e5
boogieWoogie <kimjinuk1999@gmail.com>
2022-12-17 17:37:58 +0900

    fix(OutputView) : 출력구문 수정

--- src/main/kotlin/menu/OutputView.kt
+++ src/main/kotlin/menu/OutputView.kt
@@ -18,7 +18,7 @@ class OutputView {
     }
 
     companion object {
-        const val CATEGORY = "[ 카테고리 | 한식 | 양식 | 일식 | 중식 | 아시안 ]"
+        const val CATEGORY = "카테고리"
         const val MONDAY_TO_FRIDAY = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"
         const val RESULT_OF_RECOMMENDATION = "메뉴 추천 결과입니다."
         const val FINISHED = "추천을 완료했습니다."
