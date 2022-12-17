fb36730ddea4a6d1a14133345774baec0313b5e5
changhwan <chang5hustle@gmail.com>
2022-12-17 16:49:31 +0900

    feat(menu) :  코치 이름 입력 함수 작성

--- src/main/kotlin/menu/Application.kt
+++ src/main/kotlin/menu/Application.kt
@@ -1,16 +1,25 @@
 package menu
 
+import menu.domain.MenuController
 import menu.domain.validator.InputValidator
 import menu.view.InputView
 import menu.view.OutputView
 
 private val inputView = InputView(InputValidator())
 private val outputView = OutputView()
+private val menuController = MenuController()
 fun main() {
     progressStart()
+    progressCoachNameStage()
 }
 
 private fun progressStart() {
     outputView.printStartPhrase()
+    println()
+}
+
+private fun progressCoachNameStage() {
+    outputView.printRequestCoachNamesPhrase()
+    menuController.updateCoachs(inputView.readCoachNames())
     println()
 }
--- src/main/kotlin/menu/domain/MenuController.kt
+++ src/main/kotlin/menu/domain/MenuController.kt
@@ -1,4 +1,9 @@
 package menu.domain
 
 class MenuController {
+    private var coachNames = listOf<String>()
+
+    fun updateCoachs(coachNames: List<String>) {
+        this.coachNames = coachNames
+    }
 }
