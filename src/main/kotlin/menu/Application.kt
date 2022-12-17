a62e72c73adc46d38698864e71811aa0b1c8cf2d
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2022-12-17 13:40:53 +0900

    Feat(Application): 코치 이름과 못먹는 메뉴 저장 하는 기능 추가

--- src/main/kotlin/menu/Application.kt
+++ src/main/kotlin/menu/Application.kt
@@ -5,10 +5,12 @@ import javax.swing.plaf.LabelUI
 
 fun main() {
     printStartRecommand()
-    println(inputName())
-    var name = inputName()
-    printNotRecommand(name[1])
-    println(inputMenu())
+    var coachName = inputName()
+    var cantEat = mutableMapOf<String, List<String>>()
+    coachName.forEachIndexed() { _, it ->
+        printCantEat(it)
+        cantEat[it] = inputMenu()
+    }
 }
 fun printStartRecommand() {
     println("점심 메뉴 추천을 시작합니다.")
@@ -18,7 +20,7 @@ fun inputName(): List<String> {
     var name = Console.readLine()
     return name.split(",")
 }
-fun printNotRecommand(name: String) {
+fun printCantEat(name: String) {
     println("${name}(이)가 못 먹는 메뉴를 입력해 주세요.")
 }
 fun inputMenu(): List<String> {
