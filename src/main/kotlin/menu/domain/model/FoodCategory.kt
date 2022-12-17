0d709a3aeab598299304469292d49b60451f7c7e
dlgocks1 <cksgodl1@naver.com>
2022-12-17 13:30:33 +0900

    feat : FoodCategory를 생성하는 생성자 추가
    * 해당 생성자는 카테고리 번호에 따라 생성된다.

--- src/main/kotlin/menu/domain/model/FoodCategory.kt
+++ src/main/kotlin/menu/domain/model/FoodCategory.kt
@@ -1,6 +1,7 @@
 package menu.domain.model
 
 import menu.util.*
+import java.lang.IllegalArgumentException
 
 enum class FoodCategory(foodList: List<String>) {
     KOREAN(KOREAN_FOODS),
@@ -8,5 +9,18 @@ enum class FoodCategory(foodList: List<String>) {
     CHINESE(CHINESE_FOODS),
     ASIAN(ASIAN_FOODS),
     WESTERN(WESTERN_FOODS);
+
+    companion object {
+        fun categoryOf(category: Int): FoodCategory {
+            return when (category) {
+                1 -> JAPANENSE
+                2 -> KOREAN
+                3 -> CHINESE
+                4 -> ASIAN
+                5 -> WESTERN
+                else -> throw IllegalArgumentException("잘못된 카테고리 번호입니다.")
+            }
+        }
+    }
 
 }
