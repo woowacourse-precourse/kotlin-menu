04e38ee97bd7d457f953aefaef2741bb6c950781
JunJangE <wnswkd486@gmail.com>
2022-12-17 17:54:03 +0900

    refactor: COACH_UNEATABLE_MENU_MESSAGE 수정

--- src/main/kotlin/menu/utils/Constant.kt
+++ src/main/kotlin/menu/utils/Constant.kt
@@ -3,7 +3,7 @@ package menu.utils
 object Constant {
     const val MENU_RECOMMENDATION_START_MESSAGE = "점심 메뉴 추천을 시작합니다.\n"
     const val COACH_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)\n"
-    const val COACH_UNEATABLE_MENU_MESSAGE = "%s(이) 가 못 먹는 메뉴를 입력해 주세요."
+    const val COACH_UNEATABLE_MENU_MESSAGE = "%s(이)가 못 먹는 메뉴를 입력해 주세요."
     const val MENU_RECOMMENDATION_RESULT_MESSAGE = "메뉴 추천 결과입니다."
     const val MENU_RECOMMENDATION_RESULT_FIRST_MESSAGE = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"
     const val MENU_RECOMMENDATION_RESULT_CATEGORY_MESSAGE = "[ 카테고리 | %s | %s | %s | %s | %s ]"
