9ec5967f374a5a9cafb18f3fbcaf600a1147b6f6
changhwan <chang5hustle@gmail.com>
2022-12-17 14:52:37 +0900

    feat(menu) : 사용자 입력함수 작성

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -1,4 +1,25 @@
 package menu.view
 
-class InputView {
+import camp.nextstep.edu.missionutils.Console.*
+import menu.domain.validator.InputValidator
+
+class InputView(private val inputValidator: InputValidator) {
+
+    fun readCoachNames(): List<String> {
+        return try {
+            inputValidator.getValidateCoachNames(readLine())
+        } catch (error: IllegalArgumentException) {
+            println(error.message)
+            readCoachNames()
+        }
+    }
+
+    fun readMenuBan(coachName: String): List<String> {
+        return try {
+            inputValidator.getCoachMenuBan(userInput = readLine(), coachName = coachName)
+        } catch (error: IllegalArgumentException) {
+            println(error.message)
+            readMenuBan(coachName = coachName)
+        }
+    }
 }
