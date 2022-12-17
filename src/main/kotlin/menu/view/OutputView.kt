a6165566b4222241c5a390e2be3e3f1ba3dbd68e
윤동현 <donghyunyoon81@gmail.com>
2022-12-17 13:41:26 +0900

    feat<OutputView> : printInputExcludedFood 기능 구현
    코치가 못먹는 메뉴 입력 안내를 출력하는 기능

--- src/main/kotlin/menu/view/OutputView.kt
+++ src/main/kotlin/menu/view/OutputView.kt
@@ -6,4 +6,5 @@ class OutputView {
 
     fun printInputCoachNames() = println("코치의 이름을 입력해 주세요. (, 로 구분)")
 
+    fun printInputExcludedFood(name: String) = println("$name(이)가 못 먹는 메뉴를 입력해 주세요.")
 }
