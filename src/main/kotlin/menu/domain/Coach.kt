ad1bb6563ce888b8c5a9c623ef2432c55d14e93e
krrong <khk9664@naver.com>
2022-12-17 14:51:21 +0900

    refactor(Coach): 생성자 변경

--- src/main/kotlin/menu/domain/Coach.kt
+++ src/main/kotlin/menu/domain/Coach.kt
@@ -1,15 +1,10 @@
 package menu.domain
 
-class Coach(private val name: String) {
+class Coach(private val name: String, private val unavailableFoods: List<String>) {
     private val eatenFoods = mutableListOf<String>()  // 이미 먹은 음식 리스트
-    private var unavailableFoods = listOf<String>()   // 먹지 못하는 음식 리스트
 
     fun addEatenFood(food: String) {
         eatenFoods.add(food)
-    }
-
-    fun addUnavailableFood(unavailableFoods: List<String>) {
-        this.unavailableFoods = unavailableFoods
     }
 
     fun getEatenFoods(): MutableList<String> {
@@ -22,5 +17,9 @@ class Coach(private val name: String) {
 
     fun getName(): String {
         return this.name
+    }
+
+    override fun toString(): String {
+        return name + unavailableFoods.toString()
     }
 }
