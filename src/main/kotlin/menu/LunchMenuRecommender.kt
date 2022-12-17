9b4e6c72ddcfd392ff647621de7c6ad6ee7bb4d4
EmilyCh0 <seoyeon3089@naver.com>
2022-12-17 15:48:17 +0900

    feat(LunchMenuRecommender): 각 코치의 메뉴 선택 (예외처리X)

--- src/main/kotlin/menu/LunchMenuRecommender.kt
+++ src/main/kotlin/menu/LunchMenuRecommender.kt
@@ -4,10 +4,17 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class LunchMenuRecommender(
     private val dislikeMenus: Map<String, List<String>>,
-    private val totalMenus: List<List<String>>
+    private val totalMenus: List<List<String>>,
+    private val coaches: List<String>
 ) {
-    private val recommendedMenu = mutableMapOf<String, List<String>>()
+    private val recommendedMenu = mutableMapOf<String, MutableList<String>>()
     private val categoryChosen = mutableListOf<Int>()
+
+    init {
+        coaches.forEach {
+            recommendedMenu[it] = mutableListOf()
+        }
+    }
 
     fun chooseOneWeekMenu() {
 
@@ -27,7 +34,22 @@ class LunchMenuRecommender(
         return randomNumber
     }
 
-    private fun chooseMenu(categoryMenus: List<String>) {
+    private fun chooseMenuForEachCoach(categoryMenus: List<String>) {
+        coaches.forEach {
+            while (true) {
+                val randomMenu = chooseMenu(categoryMenus)
+                // 못 먹는 메뉴인지 체크
+                val dislike = false
+                // 중복된 메뉴인지 체크
+                val duplicateMenu = false
+                if (!dislike && !duplicateMenu) {
+                    recommendedMenu[it]?.add(randomMenu)
+                }
+            }
+        }
+    }
 
+    private fun chooseMenu(categoryMenus: List<String>): String {
+        return Randoms.shuffle(categoryMenus)[0]
     }
 }
