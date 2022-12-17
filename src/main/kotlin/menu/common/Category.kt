07d007285030476d3f24496964903d0ae747e5ff
brian <briandr@naver.com>
2022-12-17 15:21:15 +0900

    fix(Category): 오타 수정

--- src/main/kotlin/menu/common/Category.kt
+++ src/main/kotlin/menu/common/Category.kt
@@ -1,6 +1,6 @@
 package menu.common
 
-enum class Category(val order: String, val category: String, menus: List<String>) {
+enum class Category(val order: String, val category: String, val menus: List<String>) {
     JAPANESE("1", "일식", listOf("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
     KOREAN("2", "한식", listOf("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
     CHINESE("3", "중식", listOf("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
