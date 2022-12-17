540ba0094d366b0d5bde32d6bb406775ab8eaa4c
choiwoosuk <wooseokchoi0508@gmail.com>
2022-12-17 17:14:03 +0900

    refactor: inline 코드로 변경

--- src/main/kotlin/menu/CategoriesRandomGenerator.kt
+++ src/main/kotlin/menu/CategoriesRandomGenerator.kt
@@ -4,8 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class CategoriesRandomGenerator :CategoriesGenerator{
     override fun generate(): Category {
-        val categories = listOf<Category>(Category.일식,Category.한식,Category.중식,Category.아시안,Category.양식)
-        val category: Category = categories[Randoms.pickNumberInRange(1, 5)-1]
-        return category
+        val categories = listOf<Category>(Category.일식, Category.한식, Category.중식, Category.아시안, Category.양식)
+        return categories[Randoms.pickNumberInRange(1, 5) - 1]
     }
 }
