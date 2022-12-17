4ec8fdcc882b4431ef43991d33189ddfc275fb75
윤동현 <donghyunyoon81@gmail.com>
2022-12-17 14:33:25 +0900

    feat<InputView> : readExcludedFoods 기능 구현
    코치가 못먹는 음식을 입력 받아 반환하는 기능

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -7,10 +7,18 @@ class InputView {
     private val outputView = OutputView()
     private val viewValidate = ViewValidate()
 
-    fun readCoachesName():List<String> {
+    fun readCoachesName(): List<String> {
         outputView.printInputCoachNames()
         val coachNames = Console.readLine()
         viewValidate.coachNamesForm(coachNames)
         return coachNames.split(",")
     }
+
+    fun readExcludedFoods(name: String): List<String> {
+        outputView.printInputExcludedFood(name)
+        val excludedFoods = Console.readLine()
+        viewValidate.excludedFoodsForm(excludedFoods)
+        return excludedFoods.split(",")
+    }
+
 }
