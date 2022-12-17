c396a46e27e2f432e8909db1e605877e1218b2d2
JunJangE <wnswkd486@gmail.com>
2022-12-17 14:13:48 +0900

    feat: coachUneatableMenuPrint 기능 추가

--- src/main/kotlin/menu/view/OutputView.kt
+++ src/main/kotlin/menu/view/OutputView.kt
@@ -1,9 +1,12 @@
 package menu.view
 
 import menu.utils.Constant
+import menu.utils.Constant.COACH_NAME_MESSAGE
+import menu.utils.Constant.COACH_UNEATABLE_MENU_MESSAGE
+import menu.utils.Constant.MENU_RECOMMENDATION_START_MESSAGE
 
 class OutputView {
-    fun menuRecommendationStartPrint() = println(Constant.MENU_RECOMMENDATION_START_MESSAGE)
-    fun coachNamePrint() = println(Constant.COACH_NAME_MESSAGE)
-    fun coachUneatableMenuPrint() = println()
+    fun menuRecommendationStartPrint() = println(MENU_RECOMMENDATION_START_MESSAGE)
+    fun coachNamePrint() = println(COACH_NAME_MESSAGE)
+    fun coachUneatableMenuPrint(name: String) = println(COACH_UNEATABLE_MENU_MESSAGE.format(name))
 }
