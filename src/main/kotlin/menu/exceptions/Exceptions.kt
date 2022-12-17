9bf55afe9cca818f358150858f459690ef6d9b15
Yerin <myheaven_b@naver.com>
2022-12-17 15:56:34 +0900

    feat: add error 메시지

--- src/main/kotlin/menu/exceptions/Exceptions.kt
+++ src/main/kotlin/menu/exceptions/Exceptions.kt
@@ -5,9 +5,9 @@ import menu.MenuCategory
 class Exceptions {
     fun validateCoachNames(input: String) {
         val inputs = input.split(",")
-        require(inputs.size == inputs.distinct().size) { throw IllegalArgumentException(WRONG_DUPLICATE_COACH_INPUT) }
-        require(inputs.all { it.length in 2..4 }) { throw IllegalArgumentException(WRONG_NAME_LENGTH_COACH_INPUT) }
-        require(inputs.size in 2..5) { throw IllegalArgumentException(WRONG_NUMBER_COACH_INPUT) }
+        require(inputs.size == inputs.distinct().size) { throw IllegalArgumentException(ERROR_MESSAGE + WRONG_DUPLICATE_COACH_INPUT) }
+        require(inputs.all { it.length in 2..4 }) { throw IllegalArgumentException(ERROR_MESSAGE + WRONG_NAME_LENGTH_COACH_INPUT) }
+        require(inputs.size in 2..5) { throw IllegalArgumentException(ERROR_MESSAGE + WRONG_NUMBER_COACH_INPUT) }
     }
 
     fun validateCoachBias(input: String) {
@@ -15,12 +15,13 @@ class Exceptions {
             return
         }
         val inputs = input.split(",")
-        require(inputs.size == inputs.distinct().size) { throw IllegalArgumentException(WRONG_DUPLICATE_FOOD_INPUT) }
-        require(inputs.size in 0..2) { throw IllegalArgumentException(WRONG_SIZE_FOOD_INPUT) }
-        require(inputs.all { MenuCategory.isValidateMenu(it) }) { throw IllegalArgumentException(WRONG_EXIST_FOOD_INPUT) }
+        require(inputs.size == inputs.distinct().size) { throw IllegalArgumentException(ERROR_MESSAGE + WRONG_DUPLICATE_FOOD_INPUT) }
+        require(inputs.size in 0..2) { throw IllegalArgumentException(ERROR_MESSAGE + WRONG_SIZE_FOOD_INPUT) }
+        require(inputs.all { MenuCategory.isValidateMenu(it) }) { throw IllegalArgumentException(ERROR_MESSAGE + WRONG_EXIST_FOOD_INPUT) }
     }
 
     companion object {
+        const val ERROR_MESSAGE = "[ERROR] "
         const val WRONG_DUPLICATE_COACH_INPUT = "코치의 이름은 중복없이 입력해야 합니다."
         const val WRONG_NAME_LENGTH_COACH_INPUT = "코치의 이름은 2글자에서 4글자 사이여야 합니다."
         const val WRONG_NUMBER_COACH_INPUT = "2명에서 5명까지의 코치가 식사할 수 있습니다."
