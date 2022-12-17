010385e8915f5abe07178d648e2c66df31092211
changhwan <chang5hustle@gmail.com>
2022-12-17 13:55:23 +0900

    feat(menu) : 코치이름 예외처리 빈스트링 관련 함수 작성

--- src/main/kotlin/menu/constants/MenuPhrases.kt
+++ src/main/kotlin/menu/constants/MenuPhrases.kt
@@ -2,5 +2,6 @@ package menu.constants
 
 object MenuPhrases {
     const val INCORRECT_COACH_NUMBER_ERROR = "[ERROR] 입력하신 코치의 수가 알맞지 않습니다."
-    const val INCORRECT_COACH_NAME_SIZE = "[ERROR] 입력하신 코치이름의 길이가 알맞지 않습니다."
+    const val INCORRECT_COACH_NAME_SIZE_ERROR = "[ERROR] 입력하신 코치이름의 길이가 알맞지 않습니다."
+    const val BLANK_COACH_NAME_ERROR = "[ERROR] 입력하신 코치 이름중 공백으로 입력된것이 있습니다"
 }
--- src/main/kotlin/menu/domain/validator/InputValidator.kt
+++ src/main/kotlin/menu/domain/validator/InputValidator.kt
@@ -1,6 +1,7 @@
 package menu.domain.validator
 
-import menu.constants.MenuPhrases.INCORRECT_COACH_NAME_SIZE
+import menu.constants.MenuPhrases.BLANK_COACH_NAME_ERROR
+import menu.constants.MenuPhrases.INCORRECT_COACH_NAME_SIZE_ERROR
 import menu.constants.MenuPhrases.INCORRECT_COACH_NUMBER_ERROR
 import menu.constants.MenuRule.MAX_COACH_NAME_SIZE
 import menu.constants.MenuRule.MAX_COACH_NUMBER
@@ -11,7 +12,8 @@ class InputValidator {
     fun getValidateCoachNames(userInput: String): List<String> {
         val processedUserInput = userInput.split(",")
         require(checkCoachNumber(processedUserInput)) { INCORRECT_COACH_NUMBER_ERROR }
-        require(checkCoachNameSize(processedUserInput)) { INCORRECT_COACH_NAME_SIZE }
+        require(checkCoachNameSize(processedUserInput)) { INCORRECT_COACH_NAME_SIZE_ERROR }
+        require(checkEmptyString(processedUserInput)) { BLANK_COACH_NAME_ERROR }
         return processedUserInput
     }
 
@@ -19,6 +21,11 @@ class InputValidator {
 
     private fun checkCoachNameSize(coachNames: List<String>): Boolean {
         for (coachName in coachNames) if (coachName.length !in MIN_COACH_NAME_SIZE..MAX_COACH_NAME_SIZE) return false
+        return true
+    }
+
+    private fun checkEmptyString(coachNames: List<String>): Boolean {
+        for (coachName in coachNames) if (coachName.isBlank()) return false
         return true
     }
 }
