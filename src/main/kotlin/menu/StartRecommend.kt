6a534f8c3276dadbdf322c0ea844f42118e33865
Jaeheon Jeong <drdd1120@naver.com>
2022-12-17 16:02:00 +0900

    <feat> 못 먹는 메뉴 2차원 리스트로 구현

--- src/main/kotlin/menu/StartRecommend.kt
+++ src/main/kotlin/menu/StartRecommend.kt
@@ -14,34 +14,36 @@ class StartRecommend {
 
         // 코치 숫자 예외처리
         if(user.size < 2 || user.size > 5){
-            throw IllegalArgumentException("코치는 최소 2명 최대 5명이 같이 식사를 할 수 있습니다.")
+            throw IllegalArgumentException("[ERROR]코치는 최소 2명 최대 5명이 같이 식사를 할 수 있습니다.")
         }
 
         // 코치 이름 예외처리
         for (i in user.indices) {
             if(user[i].length < 2 || user[i].length > 4){
-                throw IllegalArgumentException("이름 형식에 맞춰주세요(이름은 2글자에서 4글자).")
+                throw IllegalArgumentException("[ERROR]이름 형식에 맞춰주세요(이름은 2글자에서 4글자).")
             }
         }
 
         println()
+
+        // 못 먹는 메뉴 담는 배열
         var avoid = mutableListOf<MutableList<String>>()
-        for (i in user.indices) {
+
+        for (i in 0 until user.size) {
             println(user[i] + "(이)가 못 먹는 메뉴를 입력해 주세요.")
             var menu = Console.readLine()
-            if (menu == null) {
-                avoid[i].add(".")
+            if(menu == null) {
                 continue
             }
-            if(menu.contains(",")){
-                val cantEat = menu.split(",")
-                for (j in cantEat.indices) {
-                    avoid[i].add(cantEat[j])
-                }
+            if (menu.contains(",")){
+                var cantEat = menu.split(",")
+                avoid.add(cantEat as MutableList<String>)
+                continue
             }
-
-            println()
+            avoid.add(menu as MutableList<String>)
         }
+
+        println(avoid)
 
         println("메뉴 추천 결과입니다.")
         println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]")
@@ -50,10 +52,9 @@ class StartRecommend {
         val categories: MutableList<String> = mutableListOf("", "일식", "한식", "중식", "아시안", "양식")
         var diet = mutableListOf<String>()
 
-        // [ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]
+        // 카테고리 추천
         print("[ 카테고리")
         for (i in 0 until 5) {
-
             val category: String = categories.get(Randoms.pickNumberInRange(1, 5))
             diet.add(category)
             print(" | ")
@@ -61,16 +62,16 @@ class StartRecommend {
         }
         println(" ]")
 
+        // 음식 리스트
         val japan: MutableList<String> = mutableListOf("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")
         val korea: MutableList<String> = mutableListOf("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")
         val china: MutableList<String> = mutableListOf("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")
         val asian: MutableList<String> = mutableListOf("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")
         val western: MutableList<String> = mutableListOf("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니")
 
-//        println(diet)
-
+        // 식단 짜주기
         for (idx in 0 until user.size) {
-            print("[" + user[idx])
+            print("[ " + user[idx])
             var i = 0
             while (i < diet.size){
                 if (diet[i] == "일식") {
