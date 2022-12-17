39dc2d9bb874def1abe2651a731e9a99727ed304
윤동현 <donghyunyoon81@gmail.com>
2022-12-17 15:21:38 +0900

    feat<DayOfWeek> : addMenus 기능 구현
    코치들이 추천 받은 메뉴들을 추가하는 기능

--- src/main/kotlin/menu/model/DayOfWeek.kt
+++ src/main/kotlin/menu/model/DayOfWeek.kt
@@ -1,5 +1,11 @@
 package menu.model
 
 class DayOfWeek() {
+    private val category = ""
+    private var menus = listOf<String>()
+
+    fun addMenus(coachMenus:List<String>) {
+        menus = coachMenus
+    }
 
 }
