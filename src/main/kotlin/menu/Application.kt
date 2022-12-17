8e24cc3b368c934f9012d8491fe2e76c4ee6cd3c
lee989898 <wnsrud1352@gmail.com>
2022-12-17 17:26:16 +0900

    [feat](all) 전체 구현

--- src/main/kotlin/menu/Application.kt
+++ src/main/kotlin/menu/Application.kt
@@ -1,5 +1,8 @@
 package menu
 
 fun main() {
-    // TODO: 프로그램 구현
+    val menuController = MenuController()
+    menuController.standByPhase()
+    menuController.mainPhase()
+    menuController.endPhase()
 }
