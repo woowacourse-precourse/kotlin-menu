df20c02e43d47d285714e5967ee3d0350e0b5bcc
lee989898 <wnsrud1352@gmail.com>
2022-12-17 14:16:30 +0900

    [add](MenuCategoryGenerator) 랜덤 카테코리 사이즈 및 최소 카테고리 갯수 상수 추가

--- src/main/kotlin/menu/MenuCategoryGenerator.kt
+++ src/main/kotlin/menu/MenuCategoryGenerator.kt
@@ -1,18 +1,8 @@
 package menu
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class MenuCategoryGenerator {
-
-    private val menuCategory: MenuCategory = setMenuCategory()
-
-    private fun setMenuCategory(): MenuCategory {
-        return MenuCategory(
-            JAPANESE_FOOD,
-            KOREAN_FOOD,
-            CHINESE_FOOD,
-            ASIAN_FOOD,
-            WESTERN_FOOD
-        )
-    }
 
     companion object {
         private val JAPANESE_FOOD = listOf("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")
@@ -20,5 +10,8 @@ class MenuCategoryGenerator {
         private val CHINESE_FOOD = listOf("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")
         private val ASIAN_FOOD = listOf("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "'반미", "월남쌈", "분짜")
         private val WESTERN_FOOD = listOf("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니")
+
+        private const val RANDOM_MENU_CATEGORY_SIZE = 5
+        private const val MAX_WEEK_CATEGORY_COUNT = 2
     }
 }
