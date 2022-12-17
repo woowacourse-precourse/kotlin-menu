1e6a69d18b022cad59166b6b89630b66163cfc30
s9hn <tnghqn@naver.com>
2022-12-17 13:56:29 +0900

    [FEAT(printErrorMessage)] 에러메시지를 출력하는 메소드구현

--- src/main/kotlin/menu/ui/OutputView.kt
+++ src/main/kotlin/menu/ui/OutputView.kt
@@ -1,10 +1,15 @@
 package menu.ui
 
+import menu.data.ERROR_HEADER
 import menu.data.PROGRAM_START
 
 class OutputView {
     fun printInitNotice(){
         println(PROGRAM_START)
+    }
+
+    fun printErrorMessage(errorContent: String) {
+        println(ERROR_HEADER + errorContent)
     }
 
 }
