8c7b68818ecbe4ff50c0cc860334a6fae13b0dc8
jjiiin <wjdwlin4051@gmail.com>
2022-12-17 16:47:15 +0900

    refactor(InputValidator):기능 요구사항 [ERROR]문구 추가

--- src/main/kotlin/menu/InputValidator.kt
+++ src/main/kotlin/menu/InputValidator.kt
@@ -2,10 +2,10 @@ package menu
 
 class InputValidator(val userInput: String) {
     enum class ErrorMessage(val sentence: String) {
-        LENGTH_ERROR("이름은 최소 2글자 이상 최대 4글자까지 입력할 수 있습니다."),
-        NAME_SIZE_ERROR("최소 2명, 최대 5명까지 입력할 수 있습니다."),
-        MENU_SIZE_ERROR("먹지 못하는 메뉴는 최대 2개 까지 입력할 수 있습니다."),
-        OVERLAP_ERROR("코치 이름은 중복될 수 없습니다.")
+        LENGTH_ERROR("[ERROR] 이름은 최소 2글자 이상 최대 4글자까지 입력할 수 있습니다."),
+        NAME_SIZE_ERROR("[ERROR] 최소 2명, 최대 5명까지 입력할 수 있습니다."),
+        MENU_SIZE_ERROR("[ERROR] 먹지 못하는 메뉴는 최대 2개 까지 입력할 수 있습니다."),
+        OVERLAP_ERROR("[ERROR] 코치 이름은 중복될 수 없습니다.")
     }
 
     fun checkCoachNamesException(): Boolean {
