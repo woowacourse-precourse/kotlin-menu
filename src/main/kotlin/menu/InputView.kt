623b20c64712b8cb2ca69c32ca3f6b42ac0f6088
choiwoosuk <wooseokchoi0508@gmail.com>
2022-12-17 13:53:11 +0900

    feat: 각 코치들의 못먹는 음식들을 입력받는 기능 구현

--- src/main/kotlin/menu/InputView.kt
+++ src/main/kotlin/menu/InputView.kt
@@ -4,16 +4,31 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     private val inputValidation = InputValidation()
+
     fun readCoaches():List<String>{
         return try {
             val input = Console.readLine()
             inputValidation.checkNumberOfCoach(input)
             val coaches = input.split(",")
             inputValidation.checkCoachNameLength(coaches)
-            return coaches
+            coaches
         }catch (e:IllegalArgumentException){
             println(e.message)
             readCoaches()
         }
     }
+
+    fun readImpossibleFood():List<String>{
+        return try{
+            val input = Console.readLine()
+            inputValidation.checkNumberOfImpossibleFood(input)
+            val foods = input.split(",")
+            inputValidation.checkInvalidImpossibleFood(foods, listOf())
+            foods
+        }catch (e :IllegalArgumentException){
+            println(e.message)
+            readImpossibleFood()
+        }
+    }
+
 }
