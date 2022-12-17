1b8900b564463a2d9f9d9c949740ba078b08a74a
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-12-17 17:22:22 +0900

    feat(MenuController) : 기능들 연결

--- src/main/kotlin/menu/controller/MenuController.kt
+++ src/main/kotlin/menu/controller/MenuController.kt
@@ -1,6 +1,7 @@
 package menu.controller
 
 import menu.domain.RandomCategoryGenerator
+import menu.model.Flag
 import menu.model.Recommender
 import menu.view.InputView
 import menu.view.OutputView
@@ -9,8 +10,26 @@ class MenuController {
     private val inputView = InputView()
     private val outputView = OutputView()
     private val categoryGenerator = RandomCategoryGenerator()
+    private val result = mutableListOf<List<String>>()
+    private var day: Int = 1
 
-    fun init(): Recommender {
+    fun recommendMenu() {
+        val recommender = init()
+        var categoryNumber: Int
+        while (day <= 5) {
+            do {
+                categoryNumber = categoryGenerator.generate() - 1
+                val flag = recommender.selectCategory(categoryNumber)
+            } while (flag == Flag.FAIL)
+            val todayCategory = recommender.getCategory(categoryNumber)
+            val menuList = recommender.selectMenu(todayCategory)
+            result.add(menuList)
+            day++
+        }
+        outputView.printResult(recommender, result)
+    }
+
+    private fun init(): Recommender {
         val coachNames = inputView.readCoachName()
         val banFood = mutableListOf<List<String>>()
         for (name in coachNames) {
