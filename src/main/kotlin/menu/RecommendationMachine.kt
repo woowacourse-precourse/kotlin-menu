ac3d3087e92d97b04b6e25531a5d122e76f8e633
boogieWoogie <kimjinuk1999@gmail.com>
2022-12-17 15:40:58 +0900

    refactor(RecommendationMachine) : 메소드 길이 줄이기

--- src/main/kotlin/menu/RecommendationMachine.kt
+++ src/main/kotlin/menu/RecommendationMachine.kt
@@ -8,13 +8,11 @@ class RecommendationMachine {
     //TODO : private
     var thisWeekCategories = arrayListOf<Int>()
 
-    //TODO : 메소드 길이 줄이기
     fun initThisWeekCategories() {
         var categoryCnt = 0
 
         while (categoryCnt < 7) {
             val chosenCategory = Randoms.pickNumberInRange(1, 5)
-
             if (thisWeekCategories.count { category -> category == chosenCategory } < 2) {
                 categoryCnt++
                 thisWeekCategories.add(chosenCategory)
@@ -24,10 +22,10 @@ class RecommendationMachine {
 
     fun recommendMenu(eachCoach: Coach) {
         thisWeekCategories.forEach { categoryNum ->
-            val randomMenu = Randoms.shuffle(menus[categoryNum])[0]
             var isRecommended = false
 
             while (!isRecommended) {
+                val randomMenu = Randoms.shuffle(menus[categoryNum])[0]
                 isRecommended = eachCoach.selectMyMenu(randomMenu)
             }
         }
