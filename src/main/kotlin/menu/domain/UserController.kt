4a392ef8fb80281a5de7d3a33d347dcc81aa18b9
s9hn <tnghqn@naver.com>
2022-12-17 14:37:42 +0900

    [FEAT(UserController)] 프로그램 시작단계 구현부

--- src/main/kotlin/menu/domain/UserController.kt
+++ src/main/kotlin/menu/domain/UserController.kt
@@ -1,11 +1,28 @@
 package menu.domain
 
+import menu.ui.InputView
 import menu.ui.OutputView
 
 class UserController {
     private val outputView = OutputView()
+    private val inputView = InputView()
 
-    fun programRun(){
+    fun programRun() {
         outputView.printInitNotice()
+        outputView.printNoticeEnterName()
+        val coachNameContainer = inputView.enterCoachName()
+
+        for (coachName in coachNameContainer.indices) {
+            outputView.printNoticeNoEating(coachNameContainer[coachName])
+            inputView.enterNoEating()
+        }
+//        print(a.length)
+//
+//        val temp = a.split(",")
+//        print("dd$temp")
+//        for (i in temp.indices){
+//            print(i)
+//        }
+        //inputView.enterNoEating()
     }
 }
