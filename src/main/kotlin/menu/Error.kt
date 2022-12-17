510d59f8854fb7939b6619f94cfa7c914e4615d8
mendel <mendel.urssu@gmail.com>
2022-12-17 15:42:27 +0900

    refactor: 입력 유효성 검증에 쓰이는 숫자들을 상수화 시킴

--- src/main/kotlin/menu/Error.kt
+++ src/main/kotlin/menu/Error.kt
@@ -4,5 +4,6 @@ enum class Error(val message: String) {
     ERROR_COACH_PERSON_NUM_WRONG("\n[ERROR] 코치의 이름은 2명 이상 5명 이하여야 합니다."),
     ERROR_COACH_NAME_WRONG("\n[ERROR] 코치의 이름은 2글자 이상 4글자 이하여야 합니다."),
     ERROR_HATE_MENU_MAX_TWO("\n[ERROR] 코치가 못 먹는 메뉴는 최대 두 개입니다."),
-
+    ERROR_DUPLICATE_COACH_NAME("\n[ERROR] 코치의 이름에 중복이 있으면 안됩니다."),
+    ERROR_HATE_MENU_DUPLICATE("\n[ERROR] 메뉴에는 중복이 있으면 안됩니다."),
 }
--- src/main/kotlin/menu/InputValidator.kt
+++ src/main/kotlin/menu/InputValidator.kt
@@ -4,20 +4,28 @@ import java.lang.IllegalArgumentException
 
 object InputValidator {
     fun coachNames(input: String) {
-        val names = input.split(",").map { it.trim() }
-        if (names.size < 2 || names.size > 5) throwError(Error.ERROR_COACH_PERSON_NUM_WRONG)
-        names.forEach {
-            if (it.length < 2 || it.length > 4) throwError(Error.ERROR_COACH_NAME_WRONG)
+        val coaches = input.split(",").map { it.trim() }
+        if (coaches.size < MIN_COACH || coaches.size > MAX_COACH) throwError(Error.ERROR_COACH_PERSON_NUM_WRONG)
+        coaches.forEach {
+            if (it.length < MIN_NAME_LENGTH || it.length > MAX_NAME_LENGTH) throwError(Error.ERROR_COACH_NAME_WRONG)
         }
+        if(coaches.size!=coaches.distinct().size) throwError(Error.ERROR_DUPLICATE_COACH_NAME)
     }
 
     fun hateMenu(input: String) {
-        val menus = input.split(",").map { it.trim() }
-        if (menus.isEmpty()) return // 빈 것은 괜찮음. 못 먹는 음식이 없다는 의미.
-        if (menus.size > 2) throwError(Error.ERROR_HATE_MENU_MAX_TWO)
+        val hateMenus = input.split(",").map { it.trim() }
+        if (hateMenus.isEmpty()) return // 빈 것은 괜찮음. 못 먹는 음식이 없다는 의미.
+        if (hateMenus.size > MAX_HATE_FOOD) throwError(Error.ERROR_HATE_MENU_MAX_TWO)
+        if (hateMenus.size!=hateMenus.distinct().size) throwError(Error.ERROR_HATE_MENU_DUPLICATE)
     }
 
     private fun throwError(error: Error) {
         throw IllegalArgumentException(error.message)
     }
+
+    private const val MAX_COACH = 5
+    private const val MIN_COACH = 2
+    private const val MIN_NAME_LENGTH = 2
+    private const val MAX_NAME_LENGTH = 4
+    private const val MAX_HATE_FOOD = 2
 }
