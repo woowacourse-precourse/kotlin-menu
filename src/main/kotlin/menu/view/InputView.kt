fa5c615323930c5f63b976eee10463e55daad2cb
JunJangE <wnswkd486@gmail.com>
2022-12-17 13:50:48 +0900

    feat: getCoachName 기능 추가

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -1,4 +1,16 @@
 package menu.view
 
+import camp.nextstep.edu.missionutils.Console
+import menu.utils.Exception.coachNameInputException
+
+
 class InputView {
+    fun getCoachName(): List<String> {
+        while (true) {
+            val coachName = Console.readLine()!!
+            kotlin.runCatching {
+                coachName.coachNameInputException()
+            }.onSuccess { return coachName.trim().split(",") }.onFailure { println(it.message) }
+        }
+    }
 }
