6452bc79e2f4a81621961f81c4e7272e26a4f87c
EmilyCh0 <seoyeon3089@naver.com>
2022-12-17 16:22:34 +0900

    feat(OutputView): 추천 완료 문구 출력

--- src/main/kotlin/menu/OutputView.kt
+++ src/main/kotlin/menu/OutputView.kt
@@ -19,12 +19,13 @@ class OutputView {
     }
 
     fun printEndMessage() {
-
+        println(END_RECOMMENDATION)
     }
 
     companion object {
         private const val RECOMMENDATION_RESULT = "메뉴 추천 결과입니다."
         private const val DAYS_OF_A_WEEK = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"
         private const val CATEGORY = "[ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]"
+        private const val END_RECOMMENDATION = "추천을 완료했습니다."
     }
 }
