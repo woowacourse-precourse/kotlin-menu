8d5cb0839de323c1216a4e409e54eff79d5c9177
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-12-17 16:33:02 +0900

    feat : 먹었던 음식 필터 기능 추가

--- src/main/kotlin/menu/domain/MenuRecommender.kt
+++ src/main/kotlin/menu/domain/MenuRecommender.kt
@@ -4,14 +4,30 @@ import menu.util.Util
 
 class MenuRecommender {
     val coachNameList = mutableListOf<String>()
+    private var ateFoodList = mutableListOf<MutableList<String>>()
     private val hateFoodList = mutableListOf<List<String>>()
     private val category = Category()
+    val categoryList = mutableListOf<String>()
     private val food = Food()
     val weekMenu = mutableListOf<MutableList<String>>()
+
+    init {
+        initAteFoodList()
+    }
 
     fun setCoachNameList(input: String) {
         val inputSplit = Util().convertCommaStringToList(input)
         coachNameList.addAll(inputSplit)
+    }
+
+    private fun initAteFoodList() {
+        for(xIndex in 0..2){
+            val list = mutableListOf<String>()
+            for(yIndex in 0..4){
+                list.add("")
+            }
+            ateFoodList.add(list)
+        }
     }
 
     fun setHateFoodList(input: String) {
@@ -23,19 +39,20 @@ class MenuRecommender {
         for (day in 0..4) {
             val dayFood = mutableListOf<String>()
             val category = category.pickCategory()
-            for (coachList in 0 until hateFoodList.size) {
-                dayFood.add(addRecommendFood(hateFoodList[coachList],category))
-                println(dayFood)
+            categoryList.add(category)
+            for (coach in 0 until hateFoodList.size) {
+                val todayFood = addRecommendFood(hateFoodList[coach], ateFoodList[coach], category)
+                dayFood.add(todayFood)
+                ateFoodList[coach].add(todayFood)
             }
             weekMenu.add(dayFood)
         }
-        println(weekMenu)
     }
 
-    private fun addRecommendFood(hateFood: List<String>,category : String): String {
+    private fun addRecommendFood(hateFood: List<String>, ateFood: List<String>, category: String): String {
         var coachFood = food.recommendFood(category)
-        if (hateFood.contains(coachFood)) {
-            return addRecommendFood(hateFood,category)
+        if (hateFood.contains(coachFood) || ateFood.contains(coachFood)) {
+            return addRecommendFood(hateFood, ateFood, category)
         }
         return coachFood
     }
