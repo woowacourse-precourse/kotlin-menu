25c22475430399557fc190a22d4abf88f9525c53
krrong <khk9664@naver.com>
2022-12-17 16:51:42 +0900

    feat(ExceptionHandler): 코치 수, 이름 길이 예외처리

--- src/main/kotlin/menu/service/ExceptionHandler.kt
+++ src/main/kotlin/menu/service/ExceptionHandler.kt
@@ -1,7 +1,29 @@
 package menu.service
 
-object ExceptionHandler {
-    fun checkCoachNames(){
+import menu.strings.ErrorMessage
+import menu.strings.Message
 
+object ExceptionHandler {
+    fun isValidNameLength(coachName: String): Boolean {
+        val names = coachName.split(",")
+        for(name in names) {
+            try {
+                require(name.length in 2..4) { ErrorMessage.COACH_NAME }
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+                return false
+            }
+        }
+        return true
+    }
+
+    fun isValidNumber(count: Int): Boolean {
+        try {
+            require(count in 2..5) { ErrorMessage.COACH_NUMBER }
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            return false
+        }
+        return true
     }
 }
