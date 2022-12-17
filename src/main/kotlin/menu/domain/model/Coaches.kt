299e46bfeff77468998153a7be7a06c86feb4760
dlgocks1 <cksgodl1@naver.com>
2022-12-17 14:51:56 +0900

    fix : 코치 생성자 내부 오류 수정
    * scope을 잘못 설정하여 오류 발생

--- src/main/kotlin/menu/domain/model/Coaches.kt
+++ src/main/kotlin/menu/domain/model/Coaches.kt
@@ -3,8 +3,9 @@ package menu.domain.model
 class Coaches(private val coaches: List<Coach>) : List<Coach> by coaches {
 
     init {
-        require(coaches.all {
-            coaches.count { it.containsName(it.getName()) } < 2
+        
+        require(coaches.all { coach ->
+            coaches.count { it.equalName(coach.getName()) } == 1
         }) {
             "코치 이름은 중복되면 안됩니다."
         }
