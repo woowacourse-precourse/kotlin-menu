1fe459dc8dfebd8aee7406499b7ab80ba0e859b7
inseonyun <yuninseon@naver.com>
2022-12-17 13:43:13 +0900

    feat: 코치 이름 입력 input 함수 생성

--- src/main/kotlin/menu/input/InputView.kt
+++ src/main/kotlin/menu/input/InputView.kt
@@ -1,5 +1,15 @@
 package menu.input
 
+import menu.exception.CoachNameException
+
 class InputView {
     // TODO: 코치 이름 입력 받기, 해당 코치가 못 먹는 메뉴 입력 받기
+
+    fun inputCoachName(): List<String> {
+        val coaches = readLine()!!
+
+        CoachNameException(coaches)
+
+        return coaches.split(',')
+    }
 }
