2c1407fdb4f4a0dae9a2fd1442fbb121484ae37e
JunJangE <wnswkd486@gmail.com>
2022-12-17 13:50:40 +0900

    feat: coachNameInputException 기능 추가

--- src/main/kotlin/menu/utils/Exception.kt
+++ src/main/kotlin/menu/utils/Exception.kt
@@ -1,4 +1,15 @@
 package menu.utils
 
+import menu.utils.Constant.COACH_NAME_INPUT_TYPE_ERROR_MESSAGE
+
 object Exception {
+    fun String.coachNameInputException() {
+        try {
+            this.trim().split(",").forEach{
+                if (it.length !in 2..4) throw IllegalArgumentException(COACH_NAME_INPUT_TYPE_ERROR_MESSAGE)
+            }
+        } catch (e: IllegalArgumentException) {
+            throw IllegalArgumentException(COACH_NAME_INPUT_TYPE_ERROR_MESSAGE)
+        }
+    }
 }
