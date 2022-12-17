3520e8474da9f1f3d2a97a9086ce3462e0a573f1
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-12-17 15:28:12 +0900

    fix : 메뉴 string을 list로 변경

--- src/main/kotlin/menu/domain/Food.kt
+++ src/main/kotlin/menu/domain/Food.kt
@@ -1,9 +1,26 @@
 package menu.domain
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Food {
-    val japanFoodDummy = "규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼"
-    val koreaFoodDummy = "김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음"
-    val chinaFoodDummy = "깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채"
-    val asiaFoodDummy = "팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜"
-    val westernFoodDummy = "라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니"
+    val japanFood = listOf("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")
+    val koreaFood = listOf("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")
+    val chinaFood = listOf("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")
+    val asiaFood = listOf("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")
+    val westernFood = listOf("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니")
+
+    fun recommendFood(category: String): String {
+        return when (category) {
+            "한식" -> pickRandomFood(koreaFood)
+            "양식" -> pickRandomFood(westernFood)
+            "중식" -> pickRandomFood(chinaFood)
+            "일식" -> pickRandomFood(japanFood)
+            "아시안" -> pickRandomFood(asiaFood)
+            else -> ""
+        }
+    }
+
+    private fun pickRandomFood(menus: List<String>): String {
+        return Randoms.shuffle(menus)[0]
+    }
 }
