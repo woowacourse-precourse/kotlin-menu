fbe7f93c3bf55ec659da37ffc506ad08826208b5
re4rk <37167652+re4rk@users.noreply.github.com>
2022-12-17 16:46:24 +0900

    fix(menu) : 경로 이상 수정

--- src/main/kotlin/menu/lunch/view/InputView.kt
+++ src/main/kotlin/menu/lunch/view/InputView.kt
@@ -1,12 +1,12 @@
 package menu.lunch.view
 
 import camp.nextstep.edu.missionutils.Console
-import menu.lunch.domain.infinityRead
 import menu.lunch.resources.ERROR_INPUT_AT_LEAST_TWO
 import menu.lunch.resources.ERROR_INPUT_AT_MOST_FIVE
 import menu.lunch.resources.ERROR_INPUT_AT_MOST_TWO
 import menu.lunch.resources.INPUT_DISLIKE_MENU
 import menu.lunch.resources.INPUT_NAME_OF_COACHES
+import menu.utils.infinityRead
 
 class InputView {
     fun readNameOfCoaches(): List<String> {
