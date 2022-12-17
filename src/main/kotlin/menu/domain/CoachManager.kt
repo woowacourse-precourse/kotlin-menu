739699522b022b23739b3fea061ebfe183cb4ccb
krrong <khk9664@naver.com>
2022-12-17 14:51:45 +0900

    feat(CoachManager): 코치 이름과 못먹은 음식을 인자로 받는 생성자 구현

--- src/main/kotlin/menu/domain/CoachManager.kt
+++ src/main/kotlin/menu/domain/CoachManager.kt
@@ -1,5 +1,11 @@
 package menu.domain
 
-class CoachManager {
-    
+class CoachManager(coachNames: List<String>,
+                   unavailableFoods: List<List<String>>) {
+    private val coaches = mutableListOf<Coach>()
+    init {
+        for (i in coachNames.indices){
+            coaches.add(Coach(coachNames[i], unavailableFoods[i]))
+        }
+    }
 }
