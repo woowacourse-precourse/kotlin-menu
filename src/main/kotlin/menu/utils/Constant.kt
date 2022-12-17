173babaf1a9196ec796dad71050bd110311edec8
JunJangE <wnswkd486@gmail.com>
2022-12-17 16:58:15 +0900

    feat: Constant 추가

--- src/main/kotlin/menu/utils/Constant.kt
+++ src/main/kotlin/menu/utils/Constant.kt
@@ -4,6 +4,12 @@ object Constant {
     const val MENU_RECOMMENDATION_START_MESSAGE = "점심 메뉴 추천을 시작합니다.\n"
     const val COACH_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)\n"
     const val COACH_UNEATABLE_MENU_MESSAGE = "%s(이) 가 못 먹는 메뉴를 입력해 주세요."
+    const val MENU_RECOMMENDATION_RESULT_MESSAGE = "메뉴 추천 결과입니다."
+    const val MENU_RECOMMENDATION_RESULT_FIRST_MESSAGE = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"
+    const val MENU_RECOMMENDATION_RESULT_CATEGORY_MESSAGE = "[ 카테고리 | %s | %s | %s | %s | %s ]"
+    const val MENU_RECOMMENDATION_RESULT_MENU_MESSAGE = "[ %s | %s | %s | %s | %s | %s ]"
+    const val MENU_RECOMMENDATION_END_MESSAGE = "\n추천을 완료했습니다."
+
 
     const val ERROR_MESSAGE = "[ERROR] 잘못입력하셨습니다."
     const val COACH_NAME_INPUT_TYPE_ERROR_MESSAGE = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자여야 합니다."
