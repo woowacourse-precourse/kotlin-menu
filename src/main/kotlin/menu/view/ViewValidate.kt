6b148ac69c28784ad636671e3739f02b9366f0e1
윤동현 <donghyunyoon81@gmail.com>
2022-12-17 13:53:41 +0900

    feat<ViewValidate> : coachNamesForm 기능 구현
    코치의 이름들이 형식에 맞지 않으면 예외 발생

--- src/main/kotlin/menu/view/ViewValidate.kt
+++ src/main/kotlin/menu/view/ViewValidate.kt
@@ -1,4 +1,17 @@
 package menu.view
 
 class ViewValidate {
+
+    fun coachNamesForm(input: String) {
+        val names = input.split(",")
+        names.forEach { name ->
+            require(COACH_NAMES_FORM.toRegex().matches(name)) { println("[ERROR] 코치의 이름은 2글자 부터 4명글자 까지 입력해야 합니다.") }
+        }
+        require((2..4).contains(names.size)) { println("[ERROR] 코치는 최소 2명 부터 최대 5명 까지만 입력해주세요.") }
+
+    }
+
+    companion object {
+        private val COACH_NAMES_FORM = "^[ㄱ-햫]{2,4}\$"
+    }
 }
