ece191f50c022cd62aa2a38f486ae97049eb91ad
healthykim <bsbs8645@snu.ac.kr>
2022-12-17 15:55:12 +0900

    feat(ValidateService): Add function for validate hate menu

--- src/main/kotlin/menu/ValidateService.kt
+++ src/main/kotlin/menu/ValidateService.kt
@@ -14,4 +14,8 @@ class ValidateService {
 
     }
 
+    fun validateHateMenu(menus: List<String>): List<String> {
+        if(menus.size > 2) throw IllegalArgumentException("$ERROR 각 코치는 최소 0개, 최대 2개의 못 먹는 메뉴가 있다. (, 로 구분해서 입력한다.)")
+        return menus
+    }
 }
