bd84497a81cab04065639cf557ff21d5288b721f
dlgocks1 <cksgodl1@naver.com>
2022-12-17 13:40:38 +0900

    feat : 카테고리에서 랜덤 메뉴 추천 로직 추가
    + foodList -> menus 로 네이밍 변경

--- src/main/kotlin/menu/domain/model/FoodCategory.kt
+++ src/main/kotlin/menu/domain/model/FoodCategory.kt
@@ -1,14 +1,19 @@
 package menu.domain.model
 
+import camp.nextstep.edu.missionutils.Randoms
 import menu.util.*
 import java.lang.IllegalArgumentException
 
-enum class FoodCategory(foodList: List<String>) {
+enum class FoodCategory(private val menus: List<String>) {
     KOREAN(KOREAN_FOODS),
     JAPANENSE(JAPANESE_FOODS),
     CHINESE(CHINESE_FOODS),
     ASIAN(ASIAN_FOODS),
     WESTERN(WESTERN_FOODS);
+
+    fun recommand(): String {
+        return Randoms.shuffle(menus)[0]
+    }
 
     companion object {
         fun categoryOf(category: Int): FoodCategory {
