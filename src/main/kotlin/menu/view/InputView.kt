a720cd66f591f07f935e25b1f353edfe6759f5bf
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-12-17 13:35:49 +0900

    feat[menu]: 코치들의 이름을 입력받는 기능 구현

---
+++ src/main/kotlin/menu/view/InputView.kt
@@ -0,0 +1,13 @@
+package menu.view
+
+import camp.nextstep.edu.missionutils.Console
+import menu.util.Validator
+
+
+class InputView {
+
+    fun inputCoachesName() : String {
+        return Console.readLine()
+    }
+
+}
