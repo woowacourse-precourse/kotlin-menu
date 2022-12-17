418ded103ce2e1c82112de92d4f151dcc39a2989
inseonyun <yuninseon@naver.com>
2022-12-17 15:54:57 +0900

    refactor: 코치의 이름 입력 값 중 공백이 있으면 제거 해줌

--- src/main/kotlin/menu/input/InputView.kt
+++ src/main/kotlin/menu/input/InputView.kt
@@ -8,7 +8,7 @@ class InputView {
     // TODO: 코치 이름 입력 받기, 해당 코치가 못 먹는 메뉴 입력 받기
 
     fun inputCoachName(): List<String> {
-        val coaches = Console.readLine()!!
+        val coaches = Console.readLine()!!.replace(" ", "")
 
         CoachNameException(coaches)
 
