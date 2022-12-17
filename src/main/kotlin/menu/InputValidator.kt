bebc8913d8ee70c78780b26dcebd228a16ea3fc8
EmilyCh0 <seoyeon3089@naver.com>
2022-12-17 14:21:16 +0900

    feat(InputValidator): 코치 이름 입력 예외처리

--- src/main/kotlin/menu/InputValidator.kt
+++ src/main/kotlin/menu/InputValidator.kt
@@ -1,8 +1,17 @@
 package menu
 
 class InputValidator {
-    fun validateCoachNames() {
-
+    fun validateCoachNames(coachName: String): List<String> {
+        val names = coachName.split(',')
+        if (names.size !in 2..5) {
+            throw IllegalArgumentException("[ERROR] 코치는 최소 2명, 최대 5명입니다.")
+        }
+        names.forEach {
+            if (it.length !in 2..4) {
+                throw IllegalArgumentException("[ERROR] 이름의 길이는 최소 2글자 최대 4글지입니다. (잘못된 입력: ${it})")
+            }
+        }
+        return names
     }
 
     fun validateDislikeMenus() {
