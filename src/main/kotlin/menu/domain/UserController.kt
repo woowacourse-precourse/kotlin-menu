67df8cce631d0b2ce85eaa3a21dcbc5316d23546
s9hn <tnghqn@naver.com>
2022-12-17 17:15:30 +0900

    [FEAT(UserController)] 추천프로그램 종료부 구현

--- src/main/kotlin/menu/domain/UserController.kt
+++ src/main/kotlin/menu/domain/UserController.kt
@@ -6,23 +6,30 @@ import menu.ui.OutputView
 class UserController {
     private val outputView = OutputView()
     private val inputView = InputView()
+    private val randomCategoryGenerator = RandomCategoryGenerator()
+    private var coachNameContainer = listOf<String>() // 용민,세훈
+    private var noEatingContainer = mutableListOf<List<String>>() // [[마라], [민초, 오이]]
 
     fun programRun() {
         outputView.printInitNotice()
         outputView.printNoticeEnterName()
-        val coachNameContainer = inputView.enterCoachName()
+        coachNameContainer = inputView.enterCoachName()
 
         for (coachName in coachNameContainer.indices) {
             outputView.printNoticeNoEating(coachNameContainer[coachName])
-            inputView.enterNoEating()
+            noEatingContainer.add(inputView.enterNoEating())
         }
-//        print(a.length)
-//
-//        val temp = a.split(",")
-//        print("dd$temp")
-//        for (i in temp.indices){
-//            print(i)
-//        }
-        //inputView.enterNoEating()
+        programResult(coachNameContainer)
+    }
+
+    private fun programResult(coachNameContainer: List<String>) {
+        val categoryList = randomCategoryGenerator.createRandomCategory()
+        outputView.printNoticeResult()
+        outputView.printCategoryContainer(categoryList)
+        for (coachName in coachNameContainer.indices) {
+            val menuList = MenuCreator(categoryList).readCategory()
+            outputView.printFoodContainer(menuList, coachNameContainer[coachName])
+        }
+        outputView.printFinishNotice()
     }
 }
