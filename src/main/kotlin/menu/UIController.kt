6fb2f9a99d42eff06f54fae2892a5e1485463c28
healthykim <bsbs8645@snu.ac.kr>
2022-12-17 15:57:43 +0900

    feat(UIController): Add function for getting coach hate menu with error handling

--- src/main/kotlin/menu/UIController.kt
+++ src/main/kotlin/menu/UIController.kt
@@ -24,5 +24,40 @@ class UIController {
         return coaches
     }
 
+    fun getCoachHateMenu(coaches: List<String>): Map<String, List<String>> {
+        var coachHateMenu = mapOf<String, List<String>>()
+        while(coachHateMenu.size != coaches.size) {
+            try {
+                coachHateMenu = getAllCoachHateMenu(coaches, coachHateMenu)
+            } catch (e: Error) {
+                println(e.message)
+            }
+        }
+
+        return coachHateMenu
+    }
+
+    private fun getAllCoachHateMenu(coaches: List<String>, coachHateMenu: Map<String, List<String>>): Map<String, List<String>> {
+        val newCoachHateMenu = mutableMapOf<String, List<String>>()
+        newCoachHateMenu.putAll(coachHateMenu)
+        for (i in coachHateMenu.size until coaches.size) {
+            var coach = coaches[i]
+            newCoachHateMenu[coach] = getOneCoachHateMenu(coach)
+        }
+        return newCoachHateMenu
+    }
+
+    private fun getOneCoachHateMenu(name: String): List<String> {
+        var coachHateMenu = listOf<String>()
+        try {
+            while (coachHateMenu.isEmpty()) {
+                coachHateMenu = validateService.validateHateMenu(inputView.getHateMenu(name))
+            }
+        } catch(e: Error) {
+            println(e.message)
+        }
+        return coachHateMenu
+    }
+
 
 }
