c5b701737a70b8a49ec378f5b43733f8c79251b8
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-12-17 13:23:17 +0900

    feat[menu]: 코치의 이름의 길이의 범위가 맞지 않는 경우 예외 처리하는 기능 구현

--- src/main/kotlin/menu/util/Constants.kt
+++ src/main/kotlin/menu/util/Constants.kt
@@ -3,7 +3,14 @@ package menu.util
 class Constants {
 
     companion object {
+
+        const val COACH_NAME_MIN_SIZE = 2
+        const val COACH_NAME_MAX_SIZE = 4
+
+
         const val ERROR_MESSAGE = "[ERROR] "
         const val NAME_VALUE_ERROR = ERROR_MESSAGE + "한글만 입력되어야 합니다."
+        const val COACH_NAME_SIZE_ERROR = ERROR_MESSAGE + "코치의 이름은 최소 2글자 최대 4글자 이어야합니다."
+
     }
 }
--- src/main/kotlin/menu/util/Validator.kt
+++ src/main/kotlin/menu/util/Validator.kt
@@ -5,4 +5,8 @@ class Validator {
     fun checkNameValid(input : String) {
         require(input.matches("^[가-힣]*$".toRegex())) { Constants.NAME_VALUE_ERROR }
     }
+
+    fun checkNameSizeValid(input: String) {
+        require(input.length in Constants.COACH_NAME_MIN_SIZE..Constants.COACH_NAME_MAX_SIZE) { Constants.COACH_NAME_SIZE_ERROR }
+    }
 }
