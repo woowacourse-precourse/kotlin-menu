c11e536efc02d4fd16f4364a470efe8c0cc6b572
krrong <khk9664@naver.com>
2022-12-17 17:02:28 +0900

    feat(InputView): 잘못된 입력 시 해당 위치부터 재입력 받도록 구현

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -6,18 +6,20 @@ import menu.service.ExceptionHandler
 class InputView {
     fun getCoachNames(): List<String> {
         var names = ""
-        do{
+        do {
             names = Console.readLine().trim()
-        }while(!ExceptionHandler.isValidNameLength(names)
-            || !ExceptionHandler.isValidNumber(names.split(",").size))
+        } while (!ExceptionHandler.isValidNameLength(names)
+            || !ExceptionHandler.isValidNumber(names.split(",").size)
+        )
 
         return names.split(",")
     }
 
-    // TODO : 음식 입력 받을 때 예외 처리 추가해야 함
     fun getUnavailableFoods(): List<String> {
-        val foods = Console.readLine()
-        val foodNames = foods.split(",")
-        return foodNames
+        var foods = ""
+        do {
+            foods = Console.readLine()
+        } while (!ExceptionHandler.isValidUnavailableFoods(foods.split(",").size))
+        return foods.split(",")
     }
 }
