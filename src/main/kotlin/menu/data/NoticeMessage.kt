014cdc65782a3aecba4fc5ca1856943819dd9bef
s9hn <tnghqn@naver.com>
2022-12-17 17:14:00 +0900

    [FEAT(NoticeMessage)] 프로그램 완료 안내문구 상수화 추가

--- src/main/kotlin/menu/data/NoticeMessage.kt
+++ src/main/kotlin/menu/data/NoticeMessage.kt
@@ -4,9 +4,12 @@ const val PROGRAM_START = "점심 메뉴 추천을 시작합니다.\n"
 const val ENTER_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)"
 const val ENTER_NO_EATING = "(이)가 못 먹는 메뉴를 입력해 주세요."
 
-const val RECOMMEND_MENU = "메뉴 추천 결과입니다.\n" +
+const val RECOMMEND_MENU = "\n메뉴 추천 결과입니다.\n" +
         "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"
-const val PROGRAM_END = "추천을 완료했습니다."
+const val PROGRAM_END = "\n추천을 완료했습니다."
+const val LEFT = "[ "
+const val RIGHT = " ]"
+const val BAR = " | "
 
 const val KOREAN_FOOD = "한식"
 const val CHINESE_FOOD = "중식"
