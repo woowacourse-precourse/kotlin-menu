2f176117c9860626c092f48c678e4a45ccfffe38
boogieWoogie <kimjinuk1999@gmail.com>
2022-12-17 16:43:03 +0900

    refactor(OutputView) : 출력구문 리팩토링

--- src/main/kotlin/menu/OutputView.kt
+++ src/main/kotlin/menu/OutputView.kt
@@ -1,31 +1,25 @@
 package menu
 
-import menu.CategoryAndMenu.categories
+import menu.FoodData.categories
 
 class OutputView {
 
-    fun printThisWeekCategories(categoryNumbers: List<Int>) {
-        println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]")
-        print("[ 카테고리 ")
-        categoryNumbers.forEach { categoryNum ->
-            print("| ${categories[categoryNum]} ")
+    fun printResults(messageCategory: String, results : List<String>) {
+        print("[ $messageCategory ")
+        results.forEach { eachResult ->
+            print("| $eachResult ")
         }
         print("]\n")
     }
 
-    fun printCoachMenus(coach: Coach) {
-        print("[ ${coach.name} ")
-        coach.thisWeekMenus.forEach { eachMenu ->
-            print("| $eachMenu ")
-        }
-        print("]\n")
+    fun printMessage(msg: String){
+        println(msg)
     }
 
-    fun printResultRecommendation(){
-        println("메뉴 추천 결과입니다.")
-    }
-
-    fun printGameEnd(){
-        println("추천을 완료했습니다.")
+    companion object{
+        const val CATEGORY = "카테고리"
+        const val MONDAY_TO_FRIDAY = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"
+        const val RESULT_OF_RECOMMENDATION = "메뉴 추천 결과입니다."
+        const val FINISHED = "추천을 완료했습니다."
     }
 }
