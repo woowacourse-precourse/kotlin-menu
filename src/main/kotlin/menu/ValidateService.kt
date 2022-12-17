ffcbe68af3cae082cdf64f9a7859469d62b1ee9d
healthykim <bsbs8645@snu.ac.kr>
2022-12-17 17:49:56 +0900

    chore: Fix error message in ValidateService

--- src/main/kotlin/menu/ValidateService.kt
+++ src/main/kotlin/menu/ValidateService.kt
@@ -6,7 +6,8 @@ const val ERROR = "[ERROR]"
 
 class ValidateService {
     fun validateCoachName(names: List<String>): List<String> {
-        if(names.size < 2 || names.size > 5) throw IllegalArgumentException("$ERROR 코치는 최소 2명, 최대 5명까지 식사를 함께 한다.")
+        if(names.size < 2) throw IllegalArgumentException("$ERROR 코치는 최소 2명 이상 입력해야 합니다.")
+        if(names.size > 5)  throw IllegalArgumentException("$ERROR 코치는 최대 5명 이하로 입력해야 합니다.")
         for(name in names) {
             if(name.length < 2 || name.length > 4) throw IllegalArgumentException("$ERROR 코치의 이름은 최소 2글자, 최대 4글자이다.")
         }
