a006119e51b9b3abe28df05777a2e58663de4fd3
Hyemin Song <hyemdooly@gmail.com>
2022-12-17 15:34:15 +0900

    refactor(InputView): 예외처리 메소드 분할

--- src/main/kotlin/menu/InputView.kt
+++ src/main/kotlin/menu/InputView.kt
@@ -3,33 +3,48 @@ package menu
 import camp.nextstep.edu.missionutils.Console
 
 class InputView {
-    fun readCoaches(): List<Coach> {
-        println("코치의 이름을 입력해주세요. (, 로 구분)")
-        val input = Console.readLine()
-        val splitResult = input.split(",")
-        val coaches = mutableListOf<Coach>()
+
+    private fun checkCoachNamesRequirements(names: List<String>): Boolean {
         try {
-            if(splitResult.size < 2 || splitResult.size > 5) throw IllegalArgumentException("coach-numbers")
-            for (name in splitResult) {
+            if(names.size < 2 || names.size > 5) throw IllegalArgumentException("coach-numbers")
+            for (name in names) {
                 if(name.length < 2 || name.length > 4) throw IllegalArgumentException("coach-name-length")
-                coaches.add(Coach(name))
             }
         } catch(e: IllegalArgumentException) {
             when(e.message) {
                 "coach-numbers"->println("[ERROR] 코치는 최소 2명, 최대 5명까지 식사를 함께합니다.")
                 "coach-name-length"->println("[ERROR] 코치 이름은 최소 2글자, 최대 4글자입니다.")
             }
-            return readCoaches()
+            return false
+        }
+        return true
+    }
+    fun readCoaches(): List<Coach> {
+        println("코치의 이름을 입력해주세요. (, 로 구분)")
+        val input = Console.readLine().split(",")
+        val coaches = mutableListOf<Coach>()
+        if(!checkCoachNamesRequirements(input)) return readCoaches()
+        for (name in input){
+            coaches.add(Coach(name))
         }
         return coaches
     }
 
-    private fun readMenu(): List<String> {
-        val input = Console.readLine()
-        // TODO : 카테고리 속에 없는 메뉴를 예외처리할 것인가? 고민하기
-        return input.split(",")
+    private fun checkMenuNameInCategory(menu: String): Boolean {
+        return Category.전부.menus.contains(menu)
     }
-
+    private fun readMenu(): List<String> {
+        val input = Console.readLine().split(",")
+        try {
+            for(menu in input) {
+                if(!checkMenuNameInCategory(menu)) throw IllegalArgumentException()
+            }
+        }catch(e: IllegalArgumentException) {
+            println("[ERROR] 카테고리의 메뉴 리스트에 없는 메뉴입니다.")
+            return readMenu()
+        }
+        return input
+    }
 
     fun readMenus(coaches: List<Coach>) {
         for (coach in coaches) {
