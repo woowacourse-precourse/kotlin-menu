7c76b8540976d011e5fe05f39a8e3a377ecd2d13
healthykim <bsbs8645@snu.ac.kr>
2022-12-17 16:00:38 +0900

    feat(CategoryRepository): Add function for recommending category

---
+++ src/main/kotlin/menu/CategoryRepository.kt
@@ -0,0 +1,25 @@
+package menu
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class CategoryRepository {
+    private val categories: List<String> = listOf("일식", "한식", "중식", "아시안", "양식")
+    private val recommendedNumber: MutableMap<String, Int> = mutableMapOf("일식" to 0, "한식" to 0, "중식" to 0, "아시안" to 0, "양식" to 0)
+
+    private fun getRandomCategory(): String {
+        return categories[Randoms.pickNumberInRange(1, 5) - 1]
+    }
+
+    fun getRecommendCategory(): List<String> {
+        val recommendCategory = mutableListOf<String>()
+        for(i in 0..4) {
+            var recommend = getRandomCategory()
+            while(recommendedNumber[recommend] == 2) {
+                recommend = getRandomCategory()
+            }
+            recommendedNumber[recommend] = recommendedNumber[recommend]!! + 1
+            recommendCategory.add(recommend)
+        }
+        return recommendCategory
+    }
+}
