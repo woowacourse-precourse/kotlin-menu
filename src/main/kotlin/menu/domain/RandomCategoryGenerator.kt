34219f2f5a9a0a24a1752a8c556d759cf314a504
s9hn <tnghqn@naver.com>
2022-12-17 16:54:03 +0900

    [FEAT(RandomCategoryGenerator, README)] 카테고리 랜덤 생성 클래스 구현 && 기능목록 수정

---
+++ src/main/kotlin/menu/domain/RandomCategoryGenerator.kt
@@ -0,0 +1,31 @@
+package menu.domain
+
+import camp.nextstep.edu.missionutils.Randoms
+import menu.data.*
+
+class RandomCategoryGenerator {
+    private val categories: List<String> = listOf(
+        JAPANESE_FOOD,
+        KOREAN_FOOD,
+        CHINESE_FOOD,
+        ASIAN_FOOD,
+        WESTERN_FOOD
+    )
+
+
+    fun createRandomCategory(): MutableList<String> {
+        val categoryContainer = mutableListOf<String>()
+
+        while (categoryContainer.size < 5) {
+            val category = categories[Randoms.pickNumberInRange(ZERO, FOUR)]
+            categoryContainer.add(category)
+        }
+
+        return categoryContainer
+    }
+
+    companion object {
+        const val ZERO = 0
+        const val FOUR = 4
+    }
+}
