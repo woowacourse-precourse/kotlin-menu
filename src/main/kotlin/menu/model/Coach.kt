e64ac6c835f53941f335db62a991333b7533c874
vrexpert <vrexpert2302@gmail.com>
2022-12-17 16:54:34 +0900

    fix(model): 메뉴 선정 순서를 변경
    변경 전 : 월화수목금, 월화수목금
    변경 후 : 월월, 화화, 수수, 목목, 금금

--- src/main/kotlin/menu/model/Coach.kt
+++ src/main/kotlin/menu/model/Coach.kt
@@ -7,18 +7,12 @@ import java.util.*
 class Coach(val name: String, private val hateMenu: List<String>
 ) {
     val weeklyMenus = mutableListOf<String>()
-
-    fun setWeeklyMenu(categories: List<Category>) {
-        repeat(categories.size) { weekDay ->
-            weeklyMenus.add(pickMenu(categories[weekDay]))
-        }
-    }
-
-    private fun pickMenu(category: Category): String {
+    fun setWeeklyMenu(category: Category){
         while (true) {
             val menu = Randoms.shuffle(category.menus)[0]
-            if (Collections.frequency(weeklyMenus, menu) < 1) {
-                return menu
+            if (Collections.frequency(weeklyMenus, menu) < 1 && menu !in hateMenu) {
+                weeklyMenus.add(menu)
+                break
             }
         }
     }
--- src/main/kotlin/menu/model/Coaches.kt
+++ src/main/kotlin/menu/model/Coaches.kt
@@ -3,17 +3,16 @@ package menu.model
 import menu.values.Category
 
 class Coaches {
-    private val coaches = mutableListOf<Coach>()
-    fun addCoach(coach:Coach){
+    val coaches = mutableListOf<Coach>()
+    fun addCoach(coach: Coach) {
         coaches.add(coach)
     }
 
-    fun recommendCoachesMenu(weeklyCategory: List<Category>){
-        repeat(coaches.size){index->
-            coaches[index].setWeeklyMenu(weeklyCategory)
-        }
-        repeat(coaches.size){index->
-            println(coaches[index].weeklyMenus)
+    fun recommendCoachesMenu(weeklyCategory: List<Category>) {
+        repeat(weeklyCategory.size) { index ->
+            repeat(coaches.size) { coach ->
+                coaches[coach].setWeeklyMenu(weeklyCategory[index])
+            }
         }
     }
 }
