2bf3c312aa3ab55498ce35e86fe7a37660fb062f
vrexpert <vrexpert2302@gmail.com>
2022-12-17 13:45:57 +0900

    feat: 입력 예외 처리 작성

--- src/main/kotlin/menu/view/InputValidator.kt
+++ src/main/kotlin/menu/view/InputValidator.kt
@@ -1,4 +1,18 @@
 package menu.view
 
+import menu.values.ErrorMessage
+
 class InputValidator {
+    fun validateCoach(input:String){
+        val coaches = input.split(',')
+        require(coaches.size in 2..5){ErrorMessage.COACH_NUMBER_ERROR}
+        repeat(coaches.size){coach ->
+            require(coaches[coach].length in 2..4){ErrorMessage.COACH_NAME_ERROR}
+        }
+    }
+
+    fun validateHateMenu(input:String){
+        val menu = input.split(',')
+        require(menu.size in 0..2){ErrorMessage.MENU_RANGE_ERROR}
+    }
 }
