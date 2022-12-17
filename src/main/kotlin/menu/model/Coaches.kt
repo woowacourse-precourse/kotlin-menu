01e14e9bb787b0ff07b2a15efdcb12a7167ec0fa
vrexpert <vrexpert2302@gmail.com>
2022-12-17 15:49:43 +0900

    feat(model): Coaches 에게 메뉴 추천 기능 작성

--- src/main/kotlin/menu/model/Coaches.kt
+++ src/main/kotlin/menu/model/Coaches.kt
@@ -1,8 +1,19 @@
 package menu.model
 
+import menu.values.Category
+
 class Coaches {
-    val coaches = mutableListOf<Coach>()
+    private val coaches = mutableListOf<Coach>()
     fun addCoach(coach:Coach){
         coaches.add(coach)
+    }
+
+    fun recommendCoachesMenu(weeklyCategory: List<Category>){
+        repeat(coaches.size){index->
+            coaches[index].setWeeklyMenu(weeklyCategory)
+        }
+        repeat(coaches.size){index->
+            println(coaches[index].weeklyMenus)
+        }
     }
 }
