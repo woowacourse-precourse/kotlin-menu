d4c111fef10b62b2f791c51936790a5858862148
JunJangE <wnswkd486@gmail.com>
2022-12-17 16:57:15 +0900

    refactor: Category 수정

--- src/main/kotlin/menu/domain/Category.kt
+++ src/main/kotlin/menu/domain/Category.kt
@@ -1,9 +1,9 @@
 package menu.domain
 
-enum class Category(val food: Int, foodName : String) {
-    JAPANESE_FOOD(1, "일식"),
-    KOREAN_FOOD(2, "한식"),
-    CHINESE_FOOD(3, "중식"),
-    ASIAN_FOOD(4, "아시안"),
-    WESTERN_FOOD(5, "양식"),
+enum class Category(val foodName: String) {
+    JAPANESE_FOOD("일식"),
+    KOREAN_FOOD("한식"),
+    CHINESE_FOOD("중식"),
+    ASIAN_FOOD("아시안"),
+    WESTERN_FOOD("양식"),
 }
