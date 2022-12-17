d5350e04728bb6ddfc8fb86e20a05597b2c32178
inseonyun <yuninseon@naver.com>
2022-12-17 13:52:26 +0900

    feat: 못 먹는 메뉴 입력 input 함수 생성

--- src/main/kotlin/menu/input/InputView.kt
+++ src/main/kotlin/menu/input/InputView.kt
@@ -1,15 +1,25 @@
 package menu.input
 
 import menu.exception.CoachNameException
+import menu.exception.NotEatMenuException
+import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     // TODO: 코치 이름 입력 받기, 해당 코치가 못 먹는 메뉴 입력 받기
 
     fun inputCoachName(): List<String> {
-        val coaches = readLine()!!
+        val coaches = Console.readLine()!!
 
         CoachNameException(coaches)
 
         return coaches.split(',')
+    }
+
+    fun inputNotEatMenu(): List<String> {
+        val menus = Console.readLine()!!
+
+        NotEatMenuException(menus)
+
+        return menus.split(',')
     }
 }
