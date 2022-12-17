5d54845369c3d80280b3d27141c74f686cd6aa59
Sujin Kim <sujinkim919@gmail.com>
2022-12-17 14:32:43 +0900

    feat: 메뉴 추천 클래스에서 코치 이름 입력받는 기능 추가
    - 오류 발생 시 재입력

--- src/main/kotlin/menu/domain/MenuRecommendation.kt
+++ src/main/kotlin/menu/domain/MenuRecommendation.kt
@@ -7,7 +7,21 @@ class MenuRecommendation(
     private val inputView: InputView,
     private val outputView: OutputView
 ) {
+    private lateinit var coachName: List<String>
+
     fun start() {
         outputView.printStart()
+        coachName = inputCoachName()
+    }
+
+    fun inputCoachName(): List<String> {
+        while (true) {
+            try {
+                return inputView.readCoachName()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+                continue
+            }
+        }
     }
 }
--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -16,7 +16,7 @@ class InputView {
     }
 
     companion object {
-        const val INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. ($SEPARATOR 로 구분)"
-        const val INPUT_FOOD_CANT_EAT = "%s(이)가 못 먹는 메뉴를 입력해 주세요."
+        const val INPUT_COACH_NAME = "\n코치의 이름을 입력해 주세요. ($SEPARATOR 로 구분)"
+        const val INPUT_FOOD_CANT_EAT = "\n%s(이)가 못 먹는 메뉴를 입력해 주세요."
     }
 }
