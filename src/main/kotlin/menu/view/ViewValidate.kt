8f2762ae77cc32303d0ed06173ccedd524dc6194
윤동현 <donghyunyoon81@gmail.com>
2022-12-17 14:01:43 +0900

    test<ViewValidateTest> : 코치들 이름 입력 형식 유효성 테스트
    코치 이름이 2 글자에서 4글자가 아니면 예외 발생
    2명 미만으로 입력되면 예외 발생
    5명 초과로 입력되면 예외 발생

--- src/main/kotlin/menu/view/ViewValidate.kt
+++ src/main/kotlin/menu/view/ViewValidate.kt
@@ -7,11 +7,10 @@ class ViewValidate {
         names.forEach { name ->
             require(COACH_NAMES_FORM.toRegex().matches(name)) { println("[ERROR] 코치의 이름은 2글자 부터 4명글자 까지 입력해야 합니다.") }
         }
-        require((2..4).contains(names.size)) { println("[ERROR] 코치는 최소 2명 부터 최대 5명 까지만 입력해주세요.") }
-
+        require((2..5).contains(names.size)) { println("[ERROR] 코치는 최소 2명 부터 최대 5명 까지만 입력해주세요.") }
     }
 
     companion object {
-        private val COACH_NAMES_FORM = "^[ㄱ-햫]{2,4}\$"
+        private const val COACH_NAMES_FORM = "^.{2,4}\$"
     }
 }
--- src/test/kotlin/menu/ViewValidateTest.kt
+++ src/test/kotlin/menu/ViewValidateTest.kt
@@ -2,6 +2,9 @@ package menu
 
 import menu.view.ViewValidate
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class ViewValidateTest {
 
@@ -10,5 +13,13 @@ class ViewValidateTest {
     @BeforeEach
     fun setup() {
         viewValidate = ViewValidate()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["토미,제임스릴러,포코주부", "토", "토미,제임,스릴,러포,코주,부자"])
+    fun `코치들 이름 입력 형식 유효성 테스트`(input: String) {
+        assertThrows<IllegalArgumentException> {
+            viewValidate.coachNamesForm(input)
+        }
     }
 }
