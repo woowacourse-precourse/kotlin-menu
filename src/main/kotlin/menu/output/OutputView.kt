0ac2a7544c9d2474d56d5d4d742b7ea2bc7e8806
inseonyun <yuninseon@naver.com>
2022-12-17 13:59:25 +0900

    feat: 출력 함수 구현

--- src/main/kotlin/menu/output/OutputView.kt
+++ src/main/kotlin/menu/output/OutputView.kt
@@ -1,5 +1,23 @@
 package menu.output
 
+import menu.message.PrintMessage
+
 class OutputView {
     // TODO: 점심메뉴 추천 시작 문구 출력, 코치 이름 입력 문구 출력, 해당 코치가 못 먹는 메뉴 문구 출력, 메뉴 추천 결과 출력
+
+    fun startRecommendMenu() {
+        println(PrintMessage.startRecommend)
+    }
+
+    fun inputCoachName() {
+        println(PrintMessage.inputCoachName)
+    }
+
+    fun inputNotEatMenu() {
+        println(PrintMessage.inputNotEatMenu)
+    }
+
+    fun resultRecommendMenu() {
+        println(PrintMessage.resultToRecommendMenu)
+    }
 }
