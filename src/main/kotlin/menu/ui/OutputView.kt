3e8165fa0c6d261083ab583261c6cc9db113b589
s9hn <tnghqn@naver.com>
2022-12-17 14:37:07 +0900

    [FEAT(OutputView, README)] 프로그램 안내문구 출력 && 기능목록 수정
    1. 코치 이름 안내문구 출력
    2. 못 먹는 매뉴 안내문구 출력

--- src/main/kotlin/menu/ui/OutputView.kt
+++ src/main/kotlin/menu/ui/OutputView.kt
@@ -1,11 +1,21 @@
 package menu.ui
 
+import menu.data.ENTER_COACH_NAME
+import menu.data.ENTER_NO_EATING
 import menu.data.ERROR_HEADER
 import menu.data.PROGRAM_START
 
 class OutputView {
     fun printInitNotice(){
         println(PROGRAM_START)
+    }
+
+    fun printNoticeEnterName(){
+        println(ENTER_COACH_NAME)
+    }
+
+    fun printNoticeNoEating(coachName: String) {
+        println("$coachName$ENTER_NO_EATING")
     }
 
     fun printErrorMessage(errorContent: String) {
