c0bf349303c944dc1143d59c1c1cd801e8bf2821
lee989898 <wnsrud1352@gmail.com>
2022-12-17 14:15:45 +0900

    [add](MenuCategory) 메뉴 카테고리 랜덤으로 가져오는 메서드 구현

--- src/main/kotlin/menu/MenuCategory.kt
+++ src/main/kotlin/menu/MenuCategory.kt
@@ -12,6 +12,10 @@ enum class MenuCategory {
     companion object {
         private const val RANDOM_SHOULD_IN_ONE_TO_FIVE = "[ERROR] 랜덤 생성 번호는 1에서 5 사이여야 합니다."
         private const val ONE = 1
+        fun judgeMenuCategory(randomNumber: Int): String {
+            return values().find { it.ordinal == randomNumber - ONE }?.name
+                ?: throw IllegalArgumentException(RANDOM_SHOULD_IN_ONE_TO_FIVE)
+        }
     }
 }
 
