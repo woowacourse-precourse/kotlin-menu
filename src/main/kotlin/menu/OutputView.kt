377a56dfbf86c088259cacf6be3d7b586764a8c8
choiwoosuk <wooseokchoi0508@gmail.com>
2022-12-17 14:05:08 +0900

    feat: 추천 결과 출력 기능 구현

--- src/main/kotlin/menu/OutputView.kt
+++ src/main/kotlin/menu/OutputView.kt
@@ -10,6 +10,10 @@ class OutputView {
         print(SERVICE_END_MESSAGE)
     }
 
+    fun printRecommendationResult(result:String){
+        print(result)
+    }
+
     companion object{
         const val SERVICE_START_MESSAGE = "점심 메뉴 추천을 시작합니다."
         const val SERVICE_END_MESSAGE = "추천을 완료했습니다."
