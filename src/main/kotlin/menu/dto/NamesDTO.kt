47683f8c9e8b9d36c223a518ab71111f7bb926fe
Gloria <ippnsj@gmail.com>
2022-12-17 13:26:12 +0900

    feat: 코치 이름 글자수 유효성 검사 기능 구현
    코치 이름이 최소 2글자, 최대 4글자인지 확인

--- src/main/kotlin/menu/dto/NamesDTO.kt
+++ src/main/kotlin/menu/dto/NamesDTO.kt
@@ -1,4 +1,17 @@
 package menu.dto
 
-class NamesDTO(names: List<String>) {
+class NamesDTO(private val names: List<String>) {
+    init {
+        isLengthValid()
+    }
+
+    private fun isLengthValid() {
+        names.map { name -> require(name.length in MIN_LENGTH..MAX_LENGTH) { LENGTH_ERROR_MESSAGE } }
+    }
+
+    companion object {
+        const val LENGTH_ERROR_MESSAGE = "코치 이름은 최소 2글자, 최대 4글자여야 합니다."
+        const val MAX_LENGTH = 4
+        const val MIN_LENGTH = 2
+    }
 }
