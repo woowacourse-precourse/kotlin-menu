e6f31a2bc5011b99cfba120c18f7eaabec1c34ad
mendel <mendel.urssu@gmail.com>
2022-12-17 17:12:09 +0900

    style: 코틀린 코드 컨벤션에 맞게 수정

--- src/main/kotlin/menu/InputValidator.kt
+++ src/main/kotlin/menu/InputValidator.kt
@@ -13,7 +13,7 @@ object InputValidator {
     }
 
     fun hateMenu(input: String, allMenus: List<List<String>>) {
-        if(input.trim().isEmpty()) return // 빈 것은 괜찮음. 못 먹는 음식이 없다는 의미.
+        if (input.trim().isEmpty()) return // 빈 것은 괜찮음. 못 먹는 음식이 없다는 의미.
         val hateMenus = input.split(",").map { it.trim() }
         if (hateMenus.size > MAX_HATE_FOOD) throwError(Error.ERROR_HATE_MENU_MAX_TWO)
         if (hateMenus.size != hateMenus.distinct().size) throwError(Error.ERROR_HATE_MENU_DUPLICATE)
