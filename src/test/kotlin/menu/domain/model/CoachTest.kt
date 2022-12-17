1cbc32768e23e34af32b875c8163ff7d162175af
dlgocks1 <cksgodl1@naver.com>
2022-12-17 17:26:59 +0900

    test : 식단을 5개 이상 등록할 때 테스트 추가

--- src/test/kotlin/menu/domain/model/CoachTest.kt
+++ src/test/kotlin/menu/domain/model/CoachTest.kt
@@ -1,5 +1,6 @@
 package menu.domain.model
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -38,6 +39,30 @@ class CoachTest {
                 addInedible(Food.of("감차찌개"))
                 addInedible(Food.of("감차찌개"))
             }
+        }
+    }
+
+    @Test
+    @DisplayName("식단을 작성할 때 작성할 수 있는 음식인지 테스트한다.")
+    fun addFoodTest() {
+        val coach = Coach("팔코")
+        coach.addInedible(Food.of("김치찌개"))
+        coach.addDiet(Food.of("된장찌개"))
+        assertThat(coach.checkFood(Food.of("된장찌개"))).isTrue
+        assertThat(coach.checkFood(Food.of("김치찌개"))).isTrue
+        assertThat(coach.checkFood(Food.of("뇨끼"))).isFalse
+    }
+
+    @Test
+    @DisplayName("5개 이상 초과하여 식단을 등록하면 에러가 발생한다.")
+    fun dietLenghTest() {
+        val coach = Coach("팔코")
+        val testFoods = listOf("된장찌개", "짜장면", "김치찌개", "팟타이", "라자냐")
+        testFoods.forEach { foodName ->
+            coach.addDiet(Food.of(foodName))
+        }
+        assertThrows<IllegalStateException> {
+            coach.addDiet(Food.of("스파게티"))
         }
     }
 }
