0388a279d2c340ead6ab57c38dc5e35f7bd78076
jjiiin <wjdwlin4051@gmail.com>
2022-12-17 16:40:25 +0900

    feat(MenuController): 메뉴 추천 프로그램의 메인 로직 담당

---
+++ src/main/kotlin/menu/MenuController.kt
@@ -0,0 +1,72 @@
+package menu
+
+class MenuController {
+    private val outputView = OutputView()
+    private val inputView = InputView()
+    private var coachNames: List<String> = listOf()
+    private var nameMenu: MutableList<Pair<String, List<String>>> = mutableListOf()
+
+    fun main() {
+        startMessage()
+        coachNames()
+        cantEatMenu()
+        printResult()
+    }
+
+
+    fun startMessage() {
+        outputView.printStartMessage()
+    }
+
+    fun coachNames() {
+        while (true) {
+            if (inputCoachNames()) break
+        }
+    }
+
+    fun inputCoachNames(): Boolean {
+        try {
+            outputView.printInputCoachNames()
+            val userinput = inputView.inputCoachName()
+            if (InputValidator(userinput).checkCoachNamesException()) {
+                coachNames = userinput.replace(" ", "").split(',')
+            }
+        } catch (e: Exception) {
+            outputView.printError(e.message.toString())
+            return false
+        }
+        return true
+    }
+
+    fun cantEatMenu() {
+        for (name in coachNames) {
+            while (true) {
+                if (inputCantEatMenu(name)) break
+            }
+        }
+    }
+
+    fun inputCantEatMenu(name: String): Boolean {
+        try {
+            outputView.printCantEatMenu(name)
+            val userInput = inputView.inputCantEatMenu()
+            if (InputValidator(userInput).checkMenuException()) {
+                nameMenu.add(Pair(name, userInput.replace(" ", "").split(',')))
+            }
+        } catch (e: Exception) {
+            outputView.printError(e.message.toString())
+            return false
+        }
+        return true
+    }
+
+    fun printResult() {
+        val recommandMenuResult = RecommandMenuController(nameMenu)
+        recommandMenuResult.main()
+        val cateResult = recommandMenuResult.getDayCategory()
+        outputView.printCategoryResult(cateResult)
+        val result = recommandMenuResult.getFinalResult()
+        outputView.printFinalResult(Pair(coachNames, result))
+    }
+
+}
