c6d94de3a3d49ec0942fe6beb9bf2654cd38d72c
Gloria <ippnsj@gmail.com>
2022-12-17 16:13:41 +0900

    refactor: 출력 문구 형식을 맞춰주는 함수 추가로 중복 제거

--- src/main/kotlin/menu/view/OutputView.kt
+++ src/main/kotlin/menu/view/OutputView.kt
@@ -10,21 +10,26 @@ class OutputView {
 
     fun printFinish() {}
 
-
+    private fun getFormedDescription(elements: List<String>): String {
+        return elements.joinToString { DESCRIPTION_FORM }
+    }
 
     private fun printDivision() {
-        val days = Day.values().map { day -> day.byKorean() }
-        val division = days.joinToString { " | " }
-        println("[ 구분 | $division ]")
+        val days = listOf(DIVISION_HEADER) + Day.values().map { day -> day.byKorean() }
+        val description = getFormedDescription(days)
+        println("[ $description ]")
     }
 
     private fun printCategory(categories: List<Category>) {
-        val byKoreans = categories.map { category -> category.byKorean() }
-        val description = byKoreans.joinToString { " | " }
-        println("[ 카테고리 | $description ]")
+        val daysByKorean = listOf(CATEGORY_HEADER) + categories.map { category -> category.byKorean() }
+        val description = getFormedDescription(daysByKorean)
+        println("[ $description ]")
     }
 
     companion object {
+        const val CATEGORY_HEADER = "카테고리"
+        const val DESCRIPTION_FORM = " | "
+        const val DIVISION_HEADER = "구분"
         const val START_ANNOUNCEMENT = "점심 메뉴 추천을 시작합니다."
     }
 }
