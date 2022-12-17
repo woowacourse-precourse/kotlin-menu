5f09460dc1491de88abc96770690d9ce1c08ab0f
dlgocks1 <cksgodl1@naver.com>
2022-12-17 15:48:37 +0900

    feat : 메뉴 추천 결과 로직 작성
    해당 뷰에서 따로 클래스에 대한 파싱을 진행하지 않고 객체 자체가 toString()으로 출력되게 설정하였음

--- src/main/kotlin/menu/view/OutputView.kt
+++ src/main/kotlin/menu/view/OutputView.kt
@@ -1,10 +1,18 @@
 package menu.view
 
+import menu.domain.model.Coach
+import menu.domain.model.Coaches
+import menu.domain.model.WeeklyCategory
+
 class OutputView {
 
-    fun menuRecommandResults() {
+    fun menuRecommandResults(weeklyCategory: WeeklyCategory, coaches: Coaches) {
         println("메뉴 추천 결과입니다.")
-        // TODO 추후 작성
+        println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]")
+        println(weeklyCategory.toString())
+        coaches.forEach { coach ->
+            println(coach)
+        }
     }
 
     fun endRecommend() {
