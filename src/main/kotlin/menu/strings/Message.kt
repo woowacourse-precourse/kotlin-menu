b10b177d49f5a58a0700654625512c06cf3f87ed
krrong <khk9664@naver.com>
2022-12-17 13:33:29 +0900

    feat(Message): 출력 문구를 저장하는 enum class 생성

---
+++ src/main/kotlin/menu/strings/Message.kt
@@ -0,0 +1,13 @@
+package menu.strings
+
+object Message {
+    enum class PrintMessage(val msg: String) {
+        PRINT_RECOMMEND("점심 메뉴 추천을 시작합니다."),
+        GET_COACH_NAME("코치의 이름을 입력해 주세요. (, 로 구분)"),
+        GET_UNAVAILABLE_FOOD("%s(이)가 못 먹는 메뉴를 입력해 주세요."),
+        PRINT_RESULT("메뉴 추천 결과입니다."),
+        DAYS("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"),
+        CATEGORY("[ 카테고리 | %s | %s | %s | %s | %s ]"),
+        RECOMMEND_FOOD("[ %s | %s | %s | %s | %s | %s ]")
+    }
+}
