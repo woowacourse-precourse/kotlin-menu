86d8df0b17669ba41c47f32d4214328c35e9c3c5
krrong <khk9664@naver.com>
2022-12-17 17:02:08 +0900

    feat(ExceptionHandler): 못 먹는 음식의 개수 예외처리

--- src/main/kotlin/menu/service/ExceptionHandler.kt
+++ src/main/kotlin/menu/service/ExceptionHandler.kt
@@ -6,7 +6,7 @@ import menu.strings.Message
 object ExceptionHandler {
     fun isValidNameLength(coachName: String): Boolean {
         val names = coachName.split(",")
-        for(name in names) {
+        for (name in names) {
             try {
                 require(name.length in 2..4) { ErrorMessage.COACH_NAME }
             } catch (e: IllegalArgumentException) {
@@ -20,6 +20,16 @@ object ExceptionHandler {
     fun isValidNumber(count: Int): Boolean {
         try {
             require(count in 2..5) { ErrorMessage.COACH_NUMBER }
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            return false
+        }
+        return true
+    }
+
+    fun isValidUnavailableFoods(count: Int): Boolean {
+        try {
+            require(count in 0..2) { ErrorMessage.UNAVAILABLE_NUMBER }
         } catch (e: IllegalArgumentException) {
             println(e.message)
             return false
