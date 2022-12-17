1983f7dbe0d41ef24ae1df36d7b0d75d95dcc17c
Boram <lboram20@naver.com>
2022-12-17 17:12:58 +0900

    fix<MenuRecommend>: 메뉴 리스트 수정

--- src/main/kotlin/menu/MenuRecommend.kt
+++ src/main/kotlin/menu/MenuRecommend.kt
@@ -3,7 +3,7 @@ package menu
 import camp.nextstep.edu.missionutils.Randoms
 
 class MenuRecommend {
-    private val categories = listOf("일식", "한식", "중식", "아시안", "양식")
+    private val categories = listOf("메뉴", "일식", "한식", "중식", "아시안", "양식")
     private val japaneseMenu = listOf("규동","우동","미소시루","스시","가츠동","오니기리","하이라이스","라멘","오코노미야끼")
     private val koreanMenu = listOf("김밥","김치찌개","쌈밥","된장찌개","비빔밥","칼국수","불고기","떡볶이","제육볶음")
     private val chineseMenu = listOf("깐풍기","볶음면","동파육","짜장면","짬뽕","마파두부","탕수육","토마토 달걀볶음","고추잡채")
