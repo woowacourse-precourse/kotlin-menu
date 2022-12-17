b6dee29e5ef87d42ddfae573d33d5a73e316b9d7
Gloria <ippnsj@gmail.com>
2022-12-17 13:28:55 +0900

    feat: 코치 인원수 유효성 검사 기능 구현
    코치가 최소 2명, 최대 5명인지 확인

--- src/main/kotlin/menu/dto/NamesDTO.kt
+++ src/main/kotlin/menu/dto/NamesDTO.kt
@@ -3,15 +3,23 @@ package menu.dto
 class NamesDTO(private val names: List<String>) {
     init {
         isLengthValid()
+        isSizeValid()
     }
 
     private fun isLengthValid() {
         names.map { name -> require(name.length in MIN_LENGTH..MAX_LENGTH) { LENGTH_ERROR_MESSAGE } }
     }
 
+    private fun isSizeValid() {
+        require(names.size in MIN_SIZE..MAX_SIZE) { SIZE_ERROR_MESSAGE }
+    }
+
     companion object {
         const val LENGTH_ERROR_MESSAGE = "코치 이름은 최소 2글자, 최대 4글자여야 합니다."
         const val MAX_LENGTH = 4
+        const val MAX_SIZE = 5
         const val MIN_LENGTH = 2
+        const val MIN_SIZE = 2
+        const val SIZE_ERROR_MESSAGE = "코치는 최소 2명, 최대 5명이어야 합니다."
     }
 }
