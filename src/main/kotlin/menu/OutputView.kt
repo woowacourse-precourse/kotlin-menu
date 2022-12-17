bc0098d0d17d04dd2a2ad0eee1fe28a1612eadde
choiwoosuk <wooseokchoi0508@gmail.com>
2022-12-17 14:03:57 +0900

    feat: 서비스 종료 문구 출력 기능 구현

--- src/main/kotlin/menu/OutputView.kt
+++ src/main/kotlin/menu/OutputView.kt
@@ -4,10 +4,14 @@ class OutputView {
 
     fun printServiceStart(){
         println(SERVICE_START_MESSAGE)
+    }
 
+    fun printServiceEnd(){
+        print(SERVICE_END_MESSAGE)
     }
 
     companion object{
         const val SERVICE_START_MESSAGE = "점심 메뉴 추천을 시작합니다."
+        const val SERVICE_END_MESSAGE = "추천을 완료했습니다."
     }
 }
