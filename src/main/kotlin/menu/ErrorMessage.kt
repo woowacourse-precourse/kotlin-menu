a353a0f65613914019403a27b00a5db5fe63656f
SeungHyeon Shim <tladhehd@naver.com>
2022-12-17 17:02:55 +0900

    feat: 입력한 메뉴가 존재하지 않을 때 예외발생기능 추가

--- src/main/kotlin/menu/ErrorMessage.kt
+++ src/main/kotlin/menu/ErrorMessage.kt
@@ -6,4 +6,7 @@ class ErrorMessage {
     val wrongLength = Error+" 코치 이름길이는 2~4입니다."
     val wrongNumber = Error+" 코치는 최소 2명 이상 입력해야 합니다."
     val wrongMenuNum = Error+" 못먹는 메뉴는 최대 2개입니다."
+    val wrongMenu = Error+" 해당 메뉴는 없습니다."
+
+
 }
