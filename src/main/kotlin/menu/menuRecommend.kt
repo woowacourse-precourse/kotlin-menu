18db52fd07dd6a7adb43a26bffe4c9251806f7ce
SeungHyeon Shim <tladhehd@naver.com>
2022-12-17 13:53:36 +0900

    feat: input기능 추가 및 output출력 확인 , C에서 연결

---
+++ src/main/kotlin/menu/menuRecommend.kt
@@ -0,0 +1,26 @@
+package menu
+
+class menuRecommend {
+    val OV = outputView()
+    val IV = inputView()
+    val MN = Menu()
+    var coachCollect= listOf<String>()
+
+
+    constructor(){
+        OV.startService()
+        OV.inputCoach()
+        duringInput()
+    }
+
+    fun duringInput(){
+        coachCollect=IV.inputCoachName()
+        println(coachCollect)
+        for (i in 0 until coachCollect.size){
+            OV.inputNotEat(coachCollect[i])
+            IV.inputNotEating()
+            println()
+        }
+    }
+
+}
