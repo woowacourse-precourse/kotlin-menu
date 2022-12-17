c2f09999456f05b357851db845d65fcca5fa8fed
inseonyun <yuninseon@naver.com>
2022-12-17 13:42:43 +0900

    feat: 코치 이름 입력 예외 처리 클래스 생성

--- src/main/kotlin/menu/exception/CoachNameException.kt
+++ src/main/kotlin/menu/exception/CoachNameException.kt
@@ -1,5 +1,37 @@
 package menu.exception
 
-class CoachNameException {
+import menu.message.Message
+
+class CoachNameException(coaches: String) {
     // TODO: 코치 이름 입력 시 발생 되는 예외 처리
+
+    init {
+        isNotAvailableName(coaches)
+        isNotMoreThanTwo(coaches)
+        isMoreThanSix(coaches)
+    }
+
+    private fun isNotMoreThanTwo(coaches: String) {
+        val coach = coaches.split(',')
+
+        if(coach.size < 2)
+            throw IllegalArgumentException(Message.isNotMoreThanTwo)
+    }
+
+    private fun isMoreThanSix(coaches: String) {
+        val coach = coaches.split(',')
+
+        if(coach.size > 5)
+            throw IllegalArgumentException(Message.isMoreThanSix)
+    }
+
+    private fun isNotAvailableName(coaches: String) {
+        val coach = coaches.split(',')
+
+        coach.forEach {
+            if(it.length < 2 || it.length > 4) {
+                throw IllegalArgumentException(Message.isNotAvailableName)
+            }
+        }
+    }
 }
