ee432c3d48d7980cd71f7e680f72504606b27a4a
pingu244 <69796976+pingu244@users.noreply.github.com>
2022-12-17 14:10:22 +0900

    feat: 카테고리 무작위로 5개 정하기

--- src/main/kotlin/menu/controller/MenuController.kt
+++ src/main/kotlin/menu/controller/MenuController.kt
@@ -1,14 +1,17 @@
 package menu.controller
 
+import camp.nextstep.edu.missionutils.Randoms
 import menu.INSERT_NAME
 import menu.START_MESSAGE
 import menu.view.InputView
 import menu.view.OutputView
 
 class MenuController {
+
     fun start() {
         val names = startMenuRecommend()
         val cannotEatFood = askCannotEatFood(names)
+        val category = randomCategory()
 
     }
 
@@ -25,5 +28,28 @@ class MenuController {
             cannotEatFood.add(InputView().readCannotEat())
         }
         return cannotEatFood
+    }
+
+    fun randomCategory(): List<String> {
+        val determinedCategory = mutableListOf<String>()
+        val categories = listOf("일식", "한식", "중식", "아시안", "양식")
+        while (determinedCategory.size < 5) {
+            val category: String = categories[Randoms.pickNumberInRange(0, 4)]
+            if (isContainedOver2(determinedCategory, category))
+                continue
+            determinedCategory.add(category)
+        }
+        return determinedCategory
+    }
+
+    private fun isContainedOver2(determinedCategory: MutableList<String>, category: String): Boolean {
+        var count = 0
+        for (determinedOne in determinedCategory) {
+            if (determinedOne == category)
+                count++
+        }
+        if (count > 2)
+            return true
+        return false
     }
 }
