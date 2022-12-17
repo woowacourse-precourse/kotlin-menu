b0f9e79e6a1fd4814351b2c70fb6cdc48276ae2b
boogieWoogie <kimjinuk1999@gmail.com>
2022-12-17 16:20:20 +0900

    fix(InputValidator) : 여러 메뉴 입력 시 split하지 않았던 오류 수정

--- src/main/kotlin/menu/InputValidator.kt
+++ src/main/kotlin/menu/InputValidator.kt
@@ -1,36 +1,38 @@
 package menu
 
-import menu.CategoryAndMenu.isInMenus
-import menu.CategoryAndMenu.menus
+import menu.FoodData.allMenus
+import menu.FoodData.isInMenus
 
 class InputValidator {
 
-    fun validateCoaches(coachesName: String){
+    fun validateCoaches(coachesName: String) {
         coachesName.validateHasEmptyChar()
         coachesName.validateCoachesName()
         coachesName.validateNumberOfCoach()
     }
 
-    fun validateHatingMenu(hatingMenus: String){
+    fun validateHatingMenu(hatingMenus: String) {
         hatingMenus.validateHasEmptyChar()
         hatingMenus.validateNumberOfHatingMenu()
         hatingMenus.validateIsInMenu()
     }
 
-    private fun String.validateIsInMenu(){
-        require(isInMenus(this)){
-            "[ERROR] 존재하지 않는 메뉴입니다."
+    private fun String.validateIsInMenu() {
+        this.split(",").forEach { eachMenu ->
+            require(allMenus.contains(eachMenu)){
+                "[ERROR] 존재하지 않는 메뉴입니다."
+            }
         }
     }
 
-    private fun String.validateHasEmptyChar(){
-        require(!this.contains(" ")){
+    private fun String.validateHasEmptyChar() {
+        require(!this.contains(" ")) {
             "[ERROR] 공백의 문자는 허용하지 않습니다."
         }
     }
 
-    private fun String.validateNumberOfHatingMenu(){
-        require(this=="" || this.split(",").size in 0..2){
+    private fun String.validateNumberOfHatingMenu() {
+        require(this == "" || this.split(",").size in 0..2) {
             "[ERROR] 싫어하는 메뉴의 개수는 0에서 2개만 유효합니다."
         }
     }
@@ -44,7 +46,7 @@ class InputValidator {
     }
 
     private fun String.validateNumberOfCoach() {
-        require(this.split(",").size in 2..5){
+        require(this.split(",").size in 2..5) {
             "[ERROR] 식사를 하는 코치는 2명에서 5명 사이만 유효합니다."
         }
     }
