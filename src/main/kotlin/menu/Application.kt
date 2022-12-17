24fa729d1a48fa2beaea29bf274f67b5c9c7375c
hyunji1203 <hj27720@gmail.com>
2022-12-17 13:34:56 +0900

    feat: 추천할 카테고리 정하기

--- src/main/kotlin/menu/Application.kt
+++ src/main/kotlin/menu/Application.kt
@@ -4,13 +4,6 @@ import menu.UI.InputView
 import menu.UI.OutputView
 
 fun main() {
-    //메뉴
-    var japan = mutableListOf("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")
-    var korea = mutableListOf("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")
-    var china = mutableListOf("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")
-    var asian = mutableListOf("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")
-    var west = mutableListOf("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니")
-
     //시작문구
     OutputView().serviceStart()
 
@@ -29,4 +22,7 @@ fun main() {
         i.hateFood = hateFoods.toMutableList()
     }
 
+    //추천할 카테고리 정하기
+    var category = Category()
+    var weekCategory = category.recommandCategory()
 }
