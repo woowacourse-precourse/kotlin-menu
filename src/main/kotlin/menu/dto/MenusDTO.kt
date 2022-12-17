cb7ba5bf193b69c2a4f887856197184f82adf11b
Gloria <ippnsj@gmail.com>
2022-12-17 13:55:36 +0900

    feat: 각 코치가 못 먹는 메뉴 개수 유효성 검사 기능 구현
    각 코치는 최소 0개, 최대 2개의 못 먹는 메뉴가 있는지 확인

--- src/main/kotlin/menu/dto/MenusDTO.kt
+++ src/main/kotlin/menu/dto/MenusDTO.kt
@@ -1,4 +1,17 @@
 package menu.dto
 
 class MenusDTO(private val menus: List<String>) {
+    init {
+        isSizeValid()
+    }
+
+    private fun isSizeValid() {
+        require(menus.size in MIN_SIZE..MAX_SIZE) { SIZE_ERROR_MESSAGE }
+    }
+
+    companion object {
+        const val MAX_SIZE = 2
+        const val MIN_SIZE = 0
+        const val SIZE_ERROR_MESSAGE = "각 코치는 최소 0개, 최대 2개의 못 먹는 메뉴가 있어야 합니다."
+    }
 }
