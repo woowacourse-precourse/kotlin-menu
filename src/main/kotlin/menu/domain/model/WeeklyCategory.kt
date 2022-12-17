76ea766b884ae876d89dd2036dcd21b1ea45da76
dlgocks1 <cksgodl1@naver.com>
2022-12-17 13:54:41 +0900

    feat : 카테고리가 2개 초과로 중첩될 때 밸리데이션 체크

--- src/main/kotlin/menu/domain/model/WeeklyCategory.kt
+++ src/main/kotlin/menu/domain/model/WeeklyCategory.kt
@@ -2,4 +2,21 @@ package menu.domain.model
 
 class WeeklyCategory(private val weeklyCategory: List<Weekday>) : List<Weekday> by weeklyCategory {
 
+    // 한 주에 같은 카테고리는 최대 2회까지만 고를 수 있다.
+    init {
+        validateWeeklyCategory()
+    }
+
+    private fun validateWeeklyCategory() {
+        require(weeklyCategory.size == 5) {
+            "월, 화, 수, 목, 금으로 이루어진 카테고리를 입력해 주세요."
+        }
+        if (weeklyCategory.count { it.getCategory() == FoodCategory.JAPANENSE } > 2 ||
+            weeklyCategory.count { it.getCategory() == FoodCategory.KOREAN } > 2 ||
+            weeklyCategory.count { it.getCategory() == FoodCategory.JAPANENSE } > 2 ||
+            weeklyCategory.count { it.getCategory() == FoodCategory.WESTERN } > 2 ||
+            weeklyCategory.count { it.getCategory() == FoodCategory.CHINESE } > 2) {
+            throw IllegalArgumentException("카테고리가 2개 이상 중복됩니다.")
+        }
+    }
 }
