069d49e44fc454f9f12621bcb9d1735a5fb7b38b
brian <briandr@naver.com>
2022-12-17 17:10:23 +0900

    refactor(InputValidator): 상수화
    범위에 관한 숫자들을 상수화했다.

--- src/main/kotlin/menu/view/InputValidator.kt
+++ src/main/kotlin/menu/view/InputValidator.kt
@@ -3,6 +3,10 @@ package menu.view
 private const val NAME_ERROR = "코치들은 2~5명이어야합니다."
 private const val MENU_COUNT_ERROR = "못 먹는 메뉴는 0~2개여야합니다."
 private const val DUPLICATED_ERROR = "중복된 값이 있습니다."
+private const val NAME_COUNT_START = 2
+private const val NAME_COUNT_END = 5
+private const val MENU_COUNT_START = 0
+private const val MENU_COUNT_END = 0
 
 class InputValidator {
 
@@ -18,7 +22,7 @@ class InputValidator {
     }
 
     private fun checkNumberOfNames(names: List<String>) {
-        if (names.size !in 2..5) {
+        if (names.size !in NAME_COUNT_START..NAME_COUNT_END) {
             throw IllegalArgumentException(NAME_ERROR)
         }
     }
@@ -29,7 +33,7 @@ class InputValidator {
     }
 
     private fun checkNumberOfMenus(menus: List<String>) {
-        if (menus.size !in 0..2) {
+        if (menus.size !in MENU_COUNT_START..MENU_COUNT_END) {
             throw IllegalArgumentException(MENU_COUNT_ERROR)
         }
     }
