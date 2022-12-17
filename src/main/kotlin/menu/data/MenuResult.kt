14eb19a59b1b5f30e04e0a45b601db2ce2aa2ab4
Hyemin Song <hyemdooly@gmail.com>
2022-12-17 16:44:28 +0900

    refactor(MenuResult): data class로 변경

--- src/main/kotlin/menu/data/MenuResult.kt
+++ src/main/kotlin/menu/data/MenuResult.kt
@@ -1,5 +1,3 @@
 package menu.data
 
-class MenuResult(val categories: List<Category>, val recommendations: Map<String, List<String>>) {
-
-}
+data class MenuResult(val categories: List<Category>, val recommendations: Map<String, List<String>>)
