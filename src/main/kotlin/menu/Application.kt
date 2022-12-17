78b7982bb41ed45b40022fef23fe03813200b6e3
Boram <lboram20@naver.com>
2022-12-17 16:38:56 +0900

    feat<Application>: 요일별로 추천해줄 메뉴 카테고리를 선정한다.

--- src/main/kotlin/menu/Application.kt
+++ src/main/kotlin/menu/Application.kt
@@ -2,8 +2,11 @@ package menu
 
 fun main() {
     val input = InputView()
+    val menuRecommend = MenuRecommend()
 
     val dislikeMenus = mutableListOf<List<String>>()
+    val recommendedCategory = mutableListOf<String>()
+    val recommendedMenu = mutableListOf<List<String>>()
 
     println("점심 메뉴 추천을 시작합니다.")
     val coaches = input.getCoachNames()
@@ -12,5 +15,10 @@ fun main() {
         val coachName = coaches[coachIndex]
         val dislike = input.getDislikeMenu(coachName)
         dislikeMenus.add(dislike)
+    }
+
+    for(date in 0..4){ //요일별로 추천해줄 메뉴 카테고리 선정
+        val todayCategory = menuRecommend.pickCategory(recommendedCategory)
+        recommendedCategory.add(todayCategory)
     }
 }
