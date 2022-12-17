dcf0b864485bf31bd005d236e948b214e0501250
2taezeat <xorudfl772@naver.com>
2022-12-17 13:21:13 +0900

    feat(Application, InputView): 메뉴 추천을 받을 코치의 이름 입력 받기에서 출력문 추가

--- src/main/kotlin/menu/InputView.kt
+++ src/main/kotlin/menu/InputView.kt
@@ -7,8 +7,10 @@ class InputView {
 
     fun inputCoach(): String{
         var userInput = Console.readLine()
+        println(INPUT_COACH_NAME)
         while (!Validator.isValidCoachInput(userInput)) {
             println(IllegalArgumentException(INPUT_COACH_ERROR_MESSAGE))
+            println(INPUT_COACH_NAME)
             userInput = Console.readLine()
         }
         return userInput
@@ -17,6 +19,7 @@ class InputView {
 
 
     companion object {
+        const val INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)"
         const val INPUT_COACH_ERROR_MESSAGE = "[ERROR] 코치는 2~5명 까지 입력 가능하고, 코치의 이름은 2~4개의 글자여야 합니다."
     }
 
