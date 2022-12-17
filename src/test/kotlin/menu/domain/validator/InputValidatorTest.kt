3fc764149194c551950df43abc82b1ad5fffe969
changhwan <chang5hustle@gmail.com>
2022-12-17 13:47:11 +0900

    test(menu) : 코치이름 입력 예외처리 테스트

---
+++ src/test/kotlin/menu/domain/validator/InputValidatorTest.kt
@@ -0,0 +1,35 @@
+package menu.domain.validator
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class InputValidatorTest {
+
+    val validator = InputValidator()
+
+    @Nested
+    inner class CoachNameTest {
+        @Test
+        fun `코치수 2미만 일때`() {
+            assertThrows<IllegalArgumentException> { validator.getValidateCoachNames("포코") }
+        }
+
+        @Test
+        fun `코치수 5초과 일때`() {
+            assertThrows<IllegalArgumentException> { validator.getValidateCoachNames("포코,조던,월드피스,박남춘,장인성,말리빈") }
+        }
+
+        @Test
+        fun `코치이름 크기 2미만일떄 일때`() {
+            assertThrows<IllegalArgumentException> { validator.getValidateCoachNames("포코,조던,메,박남춘,장인성") }
+        }
+
+        @Test
+        fun `코치이름 크기 4초과일떄 일때`() {
+            assertThrows<IllegalArgumentException> { validator.getValidateCoachNames("포코,조던,메타월드피스,박남춘,장인성,말리빈") }
+        }
+    }
+}
