603abbb4cc405ff6e8ee6205e75c1bd74b1bedc2
s9hn <tnghqn@naver.com>
2022-12-17 13:57:38 +0900

    [FEAT(InputView, README)] 예외처리 구현 && 기능목록수정
    1. 코치의 이름이 빈값일때 예외처리
    2. 코치의 이름이 2~4글자 이외일때 예외처리

--- src/main/kotlin/menu/ui/InputView.kt
+++ src/main/kotlin/menu/ui/InputView.kt
@@ -1,14 +1,32 @@
 package menu.ui
 
 import camp.nextstep.edu.missionutils.Console
+import menu.data.ERROR_COACH_NAME
+import menu.data.ERROR_INPUT_EMPTY
+import menu.data.ERROR_RETRY
 
 class InputView {
+    private val outputView = OutputView()
 
-    fun enterCoachName(){
-        val coach = Console.readLine()
+    fun enterCoachName(): String {
+        while (true) {
+            val coachName = Console.readLine()
+            kotlin.runCatching { checkCoachName(coachName) }
+                .onSuccess { return coachName }
+                .onFailure { outputView.printErrorMessage(ERROR_RETRY) }
+        }
     }
 
-    fun enterNoEating(){
+
+    fun enterNoEating() {
         val noEating = Console.readLine()
+    }
+
+    private fun checkCoachName(coachName: String) {
+        if (coachName.length !in 2..4) {
+            throw IllegalArgumentException("${outputView.printErrorMessage(ERROR_COACH_NAME)}")
+        }
+
+        if (coachName.isBlank()) throw IllegalArgumentException("${outputView.printErrorMessage(ERROR_INPUT_EMPTY)}")
     }
 }
