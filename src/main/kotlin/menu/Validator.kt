5707f29b91f55533fcad6d2c4baf2605581cd0e5
2taezeat <xorudfl772@naver.com>
2022-12-17 13:42:59 +0900

    refactor(Validator): 하드 코딩 된 값 변경 및 접근자 변경

--- src/main/kotlin/menu/Validator.kt
+++ src/main/kotlin/menu/Validator.kt
@@ -2,41 +2,43 @@ package menu
 
 object Validator {
 
+    private const val MIN_COACH_NAME = 2
+    private const val MAX_COACH_NAME = 4
+    private const val MIN_COACH_COUNT = 2
+    private const val MAX_COACH_COUNT = 5
+
+
     fun isValidCoachInput(userInput: String): Boolean {
         if (!isInputBlank(userInput)) {
             return false
         }
         val coaches  = userInput.split(',')
-
         if (!isValidMember(coaches)) {
             return false
         }
-
         if (!isValidLetter(coaches)) {
             return false
         }
-
         return true
     }
 
-    fun isInputBlank(userInput: String): Boolean {
+    private fun isInputBlank(userInput: String): Boolean {
         if (userInput.isNullOrBlank()) return false
         return true
     }
 
-
-    fun isValidLetter(coachs: List<String>): Boolean {
+    private fun isValidLetter(coachs: List<String>): Boolean {
         for (c in coachs) {
-            if (c.length !in 2.. 4) {
+            if (c.length !in MIN_COACH_NAME.. MAX_COACH_NAME) {
                 return false
             }
         }
         return true
     }
 
-    fun isValidMember(coachs: List<String>): Boolean {
+    private fun isValidMember(coachs: List<String>): Boolean {
         val count =  coachs.size
-        if (count !in 2..5) {
+        if (count !in MIN_COACH_COUNT..MAX_COACH_COUNT) {
             return false
         }
         return true
