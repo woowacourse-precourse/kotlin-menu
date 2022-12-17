47d5c70445604ab998cb8ae3a0826de6520107a0
whk06061 <gju06061@naver.com>
2022-12-17 17:42:14 +0900

    test: 여러 번 테스트 하도록 수정

--- src/test/kotlin/menu/domain/RecommendTest.kt
+++ src/test/kotlin/menu/domain/RecommendTest.kt
@@ -11,13 +11,17 @@ internal class RecommendTest {
     fun recommendMenu() {
         val coach = Coach("혜경")
         coach.setCantEatMenus(listOf("김밥", "김치찌개"))
-        val result = recommend.recommendMenu(coach, "한식")
-        assertThat(listOf("김밥", "김치찌개")).doesNotContain(result)
+        for (i in 0 until 10) {
+            val result = recommend.recommendMenu(coach, "한식")
+            assertThat(listOf("김밥", "김치찌개")).doesNotContain(result)
+        }
     }
 
     @Test
     fun recommendCategory() {
-        val result = recommend.recommendCategory(listOf("한식", "한식", "양식"))
-        assertThat(listOf("한식", "한식")).doesNotContain(result)
+        for (i in 0 until 10) {
+            val result = recommend.recommendCategory(listOf("한식", "한식", "양식"))
+            assertThat(result).isNotEqualTo("한식")
+        }
     }
 }
