37ce3f8e3176b00295c7e33e67751d2d471b3a65
lgy15 <lgy159@naver.com>
2022-12-17 13:26:56 +0900

    feat: inputView, outputView 구현

--- src/main/kotlin/menu/View/OutputView.kt
+++ src/main/kotlin/menu/View/OutputView.kt
@@ -1,24 +1,26 @@
 package menu.View
 
+import menu.Resource.*
+
 class OutputView {
 
     fun printStartRecommend(){
-
+        print(MENU_START)
     }
 
     fun printInputName() {
-
+        print(INPUT_COACH)
     }
 
     fun printInputFood() {
-
+        print(INPUT_FOOD)
     }
 
     fun printResult() {
-
+        print(RECOMMEND_RESULT)
     }
 
     fun printEnd() {
-
+        print(RECOMMEND_END)
     }
 }
