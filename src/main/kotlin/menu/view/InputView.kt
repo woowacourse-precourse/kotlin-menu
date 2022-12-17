a35a1e7ea7ba66c793770128b847b476cf3f5db1
vrexpert <vrexpert2302@gmail.com>
2022-12-17 15:16:50 +0900

    feat(view): Input 지시문 추가

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -1,16 +1,19 @@
 package menu.view
 
 import camp.nextstep.edu.missionutils.Console
+import menu.values.GameMessage
 
-class InputView {
+class InputView (private val outputView:OutputView){
     private val validator = InputValidator()
     fun readCoach(): List<String> {
+        outputView.printMessage(GameMessage.REQUEST_COACH_NAME)
         val input = Console.readLine()
         validator.validateCoach(input)
         return input.split(',')
     }
 
     fun readHateMenu():List<String>{
+        outputView.printMessage(GameMessage.REQUEST_HATE_MENU)
        val input = Console.readLine()
         validator.validateHateMenu(input)
         return input.split(',')
