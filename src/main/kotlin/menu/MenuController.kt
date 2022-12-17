1a3d4f8383d991cff5eae7b3a401c2811304cbc8
JunJangE <wnswkd486@gmail.com>
2022-12-17 17:28:11 +0900

    feat: run, menuRecommendationStart 기능 추가

--- src/main/kotlin/menu/MenuController.kt
+++ src/main/kotlin/menu/MenuController.kt
@@ -1,6 +1,41 @@
 package menu
 
+import menu.domain.Category
+import menu.domain.CategoryChoice
+import menu.domain.Recommendation
+import menu.view.InputView
 import menu.view.OutputView
+import java.awt.Menu
 
 class MenuController {
+    fun run() {
+        OutputView().menuRecommendationStartPrint()
+        OutputView().coachNamePrint()
+        val coachName = InputView().getCoachName()
+        val coachUneatableMenu = mutableListOf<List<String>>()
+        coachName.forEach {
+            coachUneatableMenu.add(InputView().getCoachUneatableMenu(it))
+        }
+        val randomCategory = CategoryChoice().randomCategory()
+        menuRecommendationStart(coachName, randomCategory, coachUneatableMenu)
+    }
+
+    fun menuRecommendationStart(
+        coachName: List<String>,
+        randomCategory: MutableList<Category>,
+        coachUneatableMenu: MutableList<List<String>>
+    ) {
+        OutputView().menuRecommendationResultPrint()
+        OutputView().menuRecommendationResultFirstPrint()
+        OutputView().menuRecommendationResultCategoryPrint(randomCategory)
+
+        for (i in coachName.indices) {
+            val recommendationMenu = mutableListOf<String>(coachName[i])
+            for (j in 0 until 5) {
+                recommendationMenu.add(Recommendation().menuRecommendation(randomCategory[j], coachUneatableMenu[i]))
+            }
+            OutputView().menuRecommendationResultMenuPrint(recommendationMenu)
+        }
+        OutputView().menuRecommendationEPrint()
+    }
 }
