891241a125217c7159cd5b072cd9fee208e31373
Hyemin Song <hyemdooly@gmail.com>
2022-12-17 13:43:20 +0900

    feat(Coach): addUneatableMenus 수정

--- src/main/kotlin/menu/Coach.kt
+++ src/main/kotlin/menu/Coach.kt
@@ -2,7 +2,7 @@ package menu
 
 class Coach(val name: String) {
     private val uneatableMenus = mutableListOf<String>()
-    fun addUneatableMenus(menu: String) {
-        uneatableMenus.add(menu)
+    fun addUneatableMenus(menu: List<String>) {
+        uneatableMenus.addAll(menu)
     }
 }
