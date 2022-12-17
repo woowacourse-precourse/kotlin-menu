4c4e802519d7cc1ad2f03fba5244cda9914eee1f
dlgocks1 <cksgodl1@naver.com>
2022-12-17 15:22:28 +0900

    feat : 코치 인원 제한 기능 추가

--- src/main/kotlin/menu/domain/model/Coaches.kt
+++ src/main/kotlin/menu/domain/model/Coaches.kt
@@ -3,11 +3,13 @@ package menu.domain.model
 class Coaches(private val coaches: List<Coach>) : List<Coach> by coaches {
 
     init {
-        
         require(coaches.all { coach ->
             coaches.count { it.equalName(coach.getName()) } == 1
         }) {
             "코치 이름은 중복되면 안됩니다."
+        }
+        require(coaches.size in 2..5) {
+            "코치는 최소 2명, 최대 5명 입니다."
         }
     }
 }
