14a796565055b3a15d9444f3ed967176e1331577
lgy15 <lgy159@naver.com>
2022-12-17 13:54:35 +0900

    feat: input Coach Validate 구현

--- src/main/kotlin/menu/domain/Validator.kt
+++ src/main/kotlin/menu/domain/Validator.kt
@@ -2,8 +2,9 @@ package menu.domain
 
 class Validator {
     fun checkValidateCoach(coach: List<String>) {
+        require(coach.size == coach.distinct().size && coach.size >= 2 && coach.size <= 5)
         coach.forEach {
-            
+            name -> require(name.length in 2..4)
         }
     }
 }
