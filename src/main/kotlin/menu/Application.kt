fedaaa3e02fe43d1bbc93550734f290e77c448a6
Gloria <ippnsj@gmail.com>
2022-12-17 17:05:46 +0900

    feat: 애플리케이션 시작 기능 구현

--- src/main/kotlin/menu/Application.kt
+++ src/main/kotlin/menu/Application.kt
@@ -1,5 +1,23 @@
 package menu
 
+import menu.controller.RecommendController
+import menu.domain.CategoryMaker
+import menu.domain.CoachGenerator
+import menu.domain.RandomCategoryNumberGenerator
+import menu.domain.RandomMenuGenerator
+import menu.view.InputView
+import menu.view.OutputView
+
 fun main() {
-    // TODO: 프로그램 구현
+    val categoryNumberGenerator = RandomCategoryNumberGenerator()
+    val categoryMaker = CategoryMaker(categoryNumberGenerator)
+
+    val menuGenerator = RandomMenuGenerator()
+    val coachGenerator = CoachGenerator(menuGenerator)
+
+    val inputView = InputView()
+    val outputView = OutputView()
+
+    val controller = RecommendController(categoryMaker, coachGenerator, inputView, outputView)
+    controller.startService()
 }
