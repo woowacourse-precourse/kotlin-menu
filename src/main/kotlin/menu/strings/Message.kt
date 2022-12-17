822ed04c635afa8925bfa83804d84ff172012d92
krrong <khk9664@naver.com>
2022-12-17 16:19:59 +0900

    feat(Message): 메세지 추가

--- src/main/kotlin/menu/strings/Message.kt
+++ src/main/kotlin/menu/strings/Message.kt
@@ -8,7 +8,8 @@ object Message {
         PRINT_RESULT("메뉴 추천 결과입니다."),
         DAYS("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"),
         CATEGORY("[ 카테고리 | %s | %s | %s | %s | %s ]"),
-        RECOMMEND_FOOD("[ %s | %s | %s | %s | %s | %s ]")
+        RECOMMEND_FOOD("[ %s | %s | %s | %s | %s | %s ]"),
+        PRINT_END("추천을 완료했습니다.")
     }
 
     enum class Country(val value: String){
