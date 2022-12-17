f6a75415e9ded18d52cabae40bf826a5b4c39567
krrong <khk9664@naver.com>
2022-12-17 16:51:54 +0900

    refactor(ErrorMessage): 에러 메시지 수정

--- src/main/kotlin/menu/strings/ErrorMessage.kt
+++ src/main/kotlin/menu/strings/ErrorMessage.kt
@@ -2,6 +2,6 @@ package menu.strings
 
 object ErrorMessage {
     const val PREFIX = "[ERROR]"
-    const val COACH_NUMBER = "코치는 최소 2명, 최대 5명까지 식사를 함께 합니다."
-    const val COACH_NAME = "코치의 이름은 최소 2글자, 최대 4글자 입니다."
+    const val COACH_NUMBER = "[ERROR] 코치는 최소 2명, 최대 5명까지 식사를 함께 합니다."
+    const val COACH_NAME = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자 입니다."
 }
