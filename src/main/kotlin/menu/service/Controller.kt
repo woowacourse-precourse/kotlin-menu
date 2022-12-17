5ca5bbadae31eec356c3c68535b85d2fdf5f1c8b
krrong <khk9664@naver.com>
2022-12-17 17:15:54 +0900

    refactor(Controller): 함수 세분화

--- src/main/kotlin/menu/service/Controller.kt
+++ src/main/kotlin/menu/service/Controller.kt
@@ -9,19 +9,35 @@ class Controller {
     private val outputView = OutputView()
     private lateinit var logicManager: LogicManager
 
-    fun start() {
+    fun run() {
+        val coachNames = getCoachNames()
+        val unavailableFoods = getUnavailableFood(coachNames)
+        pickMenuPerCoach(coachNames, unavailableFoods)
+        showResult()
+    }
+
+    private fun getCoachNames(): List<String> {
         outputView.printStart()
         outputView.printGetCoach()
-        val coachNames = inputView.getCoachNames()
+        return inputView.getCoachNames()
+    }
+
+    private fun getUnavailableFood(coachNames: List<String>): MutableList<List<String>> {
         val unavailableFoods = mutableListOf<List<String>>()
         for (i in coachNames.indices) {
             outputView.printGetUnavailableFood(coachNames[i])
             unavailableFoods.add(inputView.getUnavailableFoods())
         }
+        return unavailableFoods
+    }
+
+    private fun pickMenuPerCoach(coachNames: List<String>, unavailableFoods: MutableList<List<String>>) {
         logicManager = LogicManager(coachNames, unavailableFoods)
         logicManager.pickCategory()
         logicManager.pickMenuFifth()
+    }
 
+    private fun showResult() {
         outputView.printResult(logicManager.getCategoriesDetails(), logicManager.getCoachesDetails())
     }
 }
