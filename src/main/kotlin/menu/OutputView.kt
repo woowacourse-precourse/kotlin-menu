6b6cab95d2afb9c783f4577b63cd7fe19314e4f1
healthykim <bsbs8645@snu.ac.kr>
2022-12-17 15:53:15 +0900

    feat(OutputView): Add function for printing recommendation

--- src/main/kotlin/menu/OutputView.kt
+++ src/main/kotlin/menu/OutputView.kt
@@ -5,5 +5,15 @@ class OutputView {
         println("점심 메뉴 추천을 시작합니다.")
     }
 
+    fun printResult(category: List<String>, recommendations: Map<String, List<String>>) {
+        println("메뉴 추천 결과입니다.")
+        println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]")
+        println("[ 카테고리 | ${category[0]} | ${category[1]} | ${category[2]} | ${category[3]} | ${category[4]} ]")
+        for(recommendation in recommendations) {
+            val menus = recommendation.value
+            println("[ ${recommendation.key} | ${recommendation.value[0]} | ${recommendation.value[1]} | ${recommendation.value[2]} | ${recommendation.value[3]} | ${recommendation.value[4]} ]")
+        }
 
+        println("추천을 완료했습니다.")
+    }
 }
