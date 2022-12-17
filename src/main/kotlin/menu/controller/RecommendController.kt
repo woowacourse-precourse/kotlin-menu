4d77b9a45946720ec7105695a1370fcd7b00b6c3
Gloria <ippnsj@gmail.com>
2022-12-17 16:46:26 +0900

    feat: 못 먹는 메뉴 가져오는 컨트롤링 기능 구현

--- src/main/kotlin/menu/controller/RecommendController.kt
+++ src/main/kotlin/menu/controller/RecommendController.kt
@@ -1,5 +1,6 @@
 package menu.controller
 
+import menu.dto.MenusDTO
 import menu.dto.NamesDTO
 import menu.view.InputView
 import menu.view.OutputView
@@ -9,6 +10,7 @@ class RecommendController(
     private val outputView: OutputView
 ) {
     private lateinit var namesDTO: NamesDTO
+    private lateinit var menusDTO: MenusDTO
 
     fun startService() {
         outputView.printStart()
@@ -24,6 +26,18 @@ class RecommendController(
         do {
             try {
                 namesDTO = inputView.readNames()
+                success = true
+            } catch (exception: IllegalArgumentException) {
+                outputView.printError(exception.message ?: "")
+            }
+        } while (!success)
+    }
+
+    private fun getMenus(name: String) {
+        var success = false
+        do {
+            try {
+                menusDTO = inputView.readMenus(name)
                 success = true
             } catch (exception: IllegalArgumentException) {
                 outputView.printError(exception.message ?: "")
