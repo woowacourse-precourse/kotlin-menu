71b07d2087f2734642259e249cd4e24925703b9b
qwe910205 <qwe910205@github.com>
2022-12-17 16:01:15 +0900

    refactor(model): 코치에 먹을 음식 테이블 속성 추가

--- src/main/kotlin/menu/model/Coach.kt
+++ src/main/kotlin/menu/model/Coach.kt
@@ -3,6 +3,7 @@ package menu.model
 class Coach(private val name: String) {
 
     private lateinit var cantEatMenus: List<Menu>
+    private val menuTable = mutableMapOf<DayOfWeek, Menu>()
 
     fun setCantEatMenus(menus: List<Menu>) {
         cantEatMenus = menus
