e39b9cbb8e51602798424a76148c0af260eba849
vrexpert <vrexpert2302@gmail.com>
2022-12-17 15:16:29 +0900

    feat: 게임 지시문 메세지 상수 작성

--- src/main/kotlin/menu/values/GameMessage.kt
+++ src/main/kotlin/menu/values/GameMessage.kt
@@ -1,4 +1,10 @@
 package menu.values
 
 object GameMessage {
+    const val START_MENU_RECOMMEND  = "점심 메뉴 추천을 시작합니다."
+    const val REQUEST_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)"
+    const val REQUEST_HATE_MENU = "%s(이)가 못 먹는 메뉴를 입력해 주세요."
+
+    const val MENU_RESULT = "메뉴 추천 결과입니다."
+    const val END = "추천을 완료했습니다."
 }
