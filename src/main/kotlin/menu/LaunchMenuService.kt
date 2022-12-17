0cce611217c71e5320791bf04e0da447655570b6
choiwoosuk <wooseokchoi0508@gmail.com>
2022-12-17 17:13:32 +0900

    feat: 중복 메뉴가 2개 있으면 다시 메뉴추천, 한 코치의 모든 메뉴를 추천하는 기능 추가

--- src/main/kotlin/menu/LaunchMenuService.kt
+++ src/main/kotlin/menu/LaunchMenuService.kt
@@ -2,40 +2,52 @@ package menu
 
 class LaunchMenuService {
 
-    fun getCategories(categoriesGenerator: CategoriesGenerator):List<Category>{
+    fun getCategories(categoriesGenerator: CategoriesGenerator): List<Category> {
         val categories = mutableListOf<Category>()
-        while(categories.size!=5){
+        while (categories.size != 5) {
             val category = categoriesGenerator.generate()
-            if(isValidCategory(categories,category)) categories.add(category)
+            if (isValidCategory(categories, category)) categories.add(category)
         }
         return categories
     }
 
-    fun isValidCategory(categories:List<Category>,category: Category):Boolean{
-        if(categories.count{presentCategory-> presentCategory == category}> MAX_DUPLICATED_CATEGORY_NUMBER) return false
+    fun isValidCategory(categories: List<Category>, category: Category): Boolean {
+        if (categories.count { presentCategory -> presentCategory == category } >= MAX_DUPLICATED_CATEGORY_NUMBER) return false
         return true
     }
 
-    fun getMenu(category: Category,impossibleMenus:List<String>,menuGenerator: MenuGenerator):String{
-        val menu = menuGenerator.generate(category.food)
-        if(isValidMenu(impossibleMenus,menu)) return menu
-        return getMenu(category,impossibleMenus,menuGenerator)
+    fun getMenus(category: List<Category>, impossibleMenus: List<String>, menuGenerator: MenuGenerator): List<String> {
+        val menus = mutableListOf<String>()
+        var index = 0
+        while (menus.size != 5) {
+            val menu = menuGenerator.generate(category[index].food)
+            if (isPossibleMenu(impossibleMenus, menu)&&isNonDuplicateMenu(menus,menu)) {
+                menus.add(menu)
+                index++
+            }
+        }
+        return menus
     }
 
-    fun isValidMenu(impossibleMenus: List<String>, menu:String):Boolean{
-        if(impossibleMenus.contains(menu)) return false
+    fun isPossibleMenu(impossibleMenus: List<String>, menu: String): Boolean {
+        if (impossibleMenus.contains(menu)) return false
         return true
     }
 
-    fun categoryToString(categories:List<Category>):List<String>{
+    fun isNonDuplicateMenu(menus:List<String>,menu:String):Boolean{
+        if(menus.contains(menu)) return false
+        return true
+    }
+
+    fun categoryToString(categories: List<Category>): List<String> {
         val categoriesToString = mutableListOf<String>()
-        categories.forEach{category->
+        categories.forEach { category ->
             categoriesToString.add(category.categoryName)
         }
         return categoriesToString
     }
 
-    companion object{
+    companion object {
         const val MAX_DUPLICATED_CATEGORY_NUMBER = 2
     }
 }
