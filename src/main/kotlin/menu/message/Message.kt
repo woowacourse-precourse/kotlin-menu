623ff7b1bf2a9b99f660048dc6a3d522ed82783e
inseonyun <yuninseon@naver.com>
2022-12-17 13:51:58 +0900

    feat: 못 먹는 메뉴 입력 예외 처리 메시지 생성

--- src/main/kotlin/menu/message/Message.kt
+++ src/main/kotlin/menu/message/Message.kt
@@ -5,5 +5,7 @@ class Message {
         const val isNotMoreThanTwo = "[ERROR] 입력된 코치가 두명 미만입니다."
         const val isMoreThanSix = "[ERROR] 입력된 코치가 여섯명 이상입니다."
         const val isNotAvailableName = "[ERROR] 입력된 코치 이름 중 최소 2글자, 최대 4글자를 지키지 못하는 값이 있습니다."
+
+        const val notEatMenuIsMoreThanThree = "[ERROR] 입력된 못 먹는 메뉴의 갯수가 3개 이상입니다."
     }
 }
