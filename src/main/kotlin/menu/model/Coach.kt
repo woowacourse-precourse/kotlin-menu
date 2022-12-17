98c6adc0049bf9ba985f7e22f83238b60d0ca654
qwe910205 <qwe910205@github.com>
2022-12-17 17:53:09 +0900

    feat(model): Coach 클래스의 검증 로직 추가
    초기화 할 때와 못 먹는 메뉴 추가할 때 검증 로직 추가

--- src/main/kotlin/menu/model/Coach.kt
+++ src/main/kotlin/menu/model/Coach.kt
@@ -1,18 +1,28 @@
 package menu.model
 
 import menu.common.MAX_CANT_EAT_MENU_SIZE
+import menu.common.MAX_COACH_NAME_SIZE
+import menu.common.MIN_COACH_NAME_SIZE
 
 class Coach(private val name: String) {
 
     private val cantEatMenus = mutableListOf<Menu>()
     private val menuTable = mutableMapOf<DayOfWeek, Menu>()
 
+    init {
+        require(name.length in MIN_COACH_NAME_SIZE..MAX_COACH_NAME_SIZE) { "코치의 이름의 길이는 2 이상 4 이하여야 합니다." }
+    }
+
     fun name(): String = name
 
-    fun addCantEatMenu(menu: Menu) = cantEatMenus.add(menu)
+    fun addCantEatMenu(menu: Menu) {
+        require(!cantEatMenus.contains(menu)) { "이미 못 먹는 메뉴입니다." }
+        cantEatMenus.add(menu)
+    }
 
     fun recommendedTooManyCategory(category: Category): Boolean =
         menuTable.values.count { it.getCategory() == category } >= MAX_CANT_EAT_MENU_SIZE
+
     fun cantEat(menu: Menu): Boolean = cantEatMenus.contains(menu)
 
     fun recommended(menu: Menu): Boolean = menuTable.values.contains(menu)
