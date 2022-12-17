0ec7a474d2f0757941c3737d5f3f5c725f1d2efb
krrong <khk9664@naver.com>
2022-12-17 16:36:00 +0900

    feat(ErrorMessage): 에러 메시지를 담당하는 클래스 생성

---
+++ src/main/kotlin/menu/strings/ErrorMessage.kt
@@ -0,0 +1,7 @@
+package menu.strings
+
+object ErrorMessage {
+    const val PREFIX = "[ERROR]"
+    const val COACH_NUMBER = "코치는 최소 2명, 최대 5명까지 식사를 함께 합니다."
+    const val COACH_NAME = "코치의 이름은 최소 2글자, 최대 4글자 입니다."
+}
