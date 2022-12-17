1e8be1a7211e27af7bb7361450511ea73ebc8df2
JunJangE <wnswkd486@gmail.com>
2022-12-17 16:58:44 +0900

    feat: OutputView 결과 기능 추가

--- src/main/kotlin/menu/view/OutputView.kt
+++ src/main/kotlin/menu/view/OutputView.kt
@@ -1,12 +1,44 @@
 package menu.view
 
-import menu.utils.Constant
+import menu.domain.Category
 import menu.utils.Constant.COACH_NAME_MESSAGE
 import menu.utils.Constant.COACH_UNEATABLE_MENU_MESSAGE
+import menu.utils.Constant.MENU_RECOMMENDATION_END_MESSAGE
+import menu.utils.Constant.MENU_RECOMMENDATION_RESULT_CATEGORY_MESSAGE
+import menu.utils.Constant.MENU_RECOMMENDATION_RESULT_FIRST_MESSAGE
+import menu.utils.Constant.MENU_RECOMMENDATION_RESULT_MENU_MESSAGE
+import menu.utils.Constant.MENU_RECOMMENDATION_RESULT_MESSAGE
 import menu.utils.Constant.MENU_RECOMMENDATION_START_MESSAGE
 
 class OutputView {
     fun menuRecommendationStartPrint() = println(MENU_RECOMMENDATION_START_MESSAGE)
     fun coachNamePrint() = println(COACH_NAME_MESSAGE)
     fun coachUneatableMenuPrint(name: String) = println(COACH_UNEATABLE_MENU_MESSAGE.format(name))
+
+    fun menuRecommendationResultPrint() = println(MENU_RECOMMENDATION_RESULT_MESSAGE)
+
+    fun menuRecommendationResultFirstPrint() = println(MENU_RECOMMENDATION_RESULT_FIRST_MESSAGE)
+    fun menuRecommendationResultCategoryPrint(category: List<Category>) = println(
+        MENU_RECOMMENDATION_RESULT_CATEGORY_MESSAGE.format(
+            category[0].foodName,
+            category[1].foodName,
+            category[2].foodName,
+            category[3].foodName,
+            category[4].foodName
+        )
+    )
+
+    fun menuRecommendationResultMenuPrint(menu: List<String>) = println(
+        MENU_RECOMMENDATION_RESULT_MENU_MESSAGE.format(
+            menu[0],
+            menu[1],
+            menu[2],
+            menu[3],
+            menu[4],
+            menu[5]
+        )
+    )
+    fun menuRecommendationEPrint() = println(MENU_RECOMMENDATION_END_MESSAGE)
+
+
 }
