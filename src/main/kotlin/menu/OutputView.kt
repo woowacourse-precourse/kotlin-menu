66a824e9166b69d44fc68f849327768306aaf818
healthykim <bsbs8645@snu.ac.kr>
2022-12-17 16:22:05 +0900

    fix(OutputView): Fix printing format in OutputView.kt

--- src/main/kotlin/menu/OutputView.kt
+++ src/main/kotlin/menu/OutputView.kt
@@ -2,11 +2,11 @@ package menu
 
 class OutputView {
     fun printStart() {
-        println("점심 메뉴 추천을 시작합니다.")
+        println("점심 메뉴 추천을 시작합니다.\n")
     }
 
     fun printResult(category: List<String>, recommendations: Map<String, List<String>>) {
-        println("메뉴 추천 결과입니다.")
+        println("\n메뉴 추천 결과입니다.")
         println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]")
         println("[ 카테고리 | ${category[0]} | ${category[1]} | ${category[2]} | ${category[3]} | ${category[4]} ]")
         for(recommendation in recommendations) {
