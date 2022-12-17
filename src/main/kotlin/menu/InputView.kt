e4ff5334ec278ffad274ab5099eaa54e6cfe1019
EmilyCh0 <seoyeon3089@naver.com>
2022-12-17 17:14:39 +0900

    refactor(InputView): 하드코딩 문자열 상수 분리

--- src/main/kotlin/menu/InputView.kt
+++ src/main/kotlin/menu/InputView.kt
@@ -4,12 +4,17 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     fun readCoachNames(): String {
-        println("코치의 이름을 입력해 주세요. (, 로 구분)")
+        println(READ_COACH_NAME_PROMPT)
         return Console.readLine()
     }
 
     fun readDislikeMenus(name: String): String {
-        println("${name}(이)가 못 먹는 메뉴를 입력해 주세요.")
+        println(READ_DISLIKE_MENU_PROMPT.format(name))
         return Console.readLine()
+    }
+
+    companion object {
+        private const val READ_COACH_NAME_PROMPT = "코치의 이름을 입력해 주세요. (, 로 구분)"
+        private const val READ_DISLIKE_MENU_PROMPT = "%s(이)가 못 먹는 메뉴를 입력해 주세요."
     }
 }
