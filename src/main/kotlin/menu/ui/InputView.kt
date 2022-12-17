55b5b196430de02ae4bf1d59235119224f057615
s9hn <tnghqn@naver.com>
2022-12-17 13:37:47 +0900

    [FEAT(InputView, README)] 코치이름 및 못먹는 매뉴 입력문 구현 && 기능목록 수정

---
+++ src/main/kotlin/menu/ui/InputView.kt
@@ -0,0 +1,14 @@
+package menu.ui
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputView {
+
+    fun enterCoachName(){
+        val coach = Console.readLine()
+    }
+
+    fun enterNoEating(){
+        val noEating = Console.readLine()
+    }
+}
