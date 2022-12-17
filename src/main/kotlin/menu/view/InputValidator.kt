0896deca3a337546b9894086b4294ee51056dc99
vrexpert <vrexpert2302@gmail.com>
2022-12-17 17:59:07 +0900

    fix: 공백 예외 처리 수정 추가

--- src/main/kotlin/menu/view/InputValidator.kt
+++ src/main/kotlin/menu/view/InputValidator.kt
@@ -4,6 +4,7 @@ import menu.values.ErrorMessage
 
 class InputValidator {
     fun validateCoach(input: String) {
+        require(input != ""){"[ERROR] 공백을 입력할 수 없습니다."}
         require(input[0] != ',' && input[input.length-1] !=','){ErrorMessage.NOT_COMMA_FRONT_BACK}
         val coaches = input.split(',')
         require(coaches.size in 2..5) { ErrorMessage.COACH_NUMBER_ERROR }
