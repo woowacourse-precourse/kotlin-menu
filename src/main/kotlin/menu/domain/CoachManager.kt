7ea817b13d405636c08da48e7cccafc66d8ffb32
krrong <khk9664@naver.com>
2022-12-17 15:19:04 +0900

    feat(CoachManager): 카테고리를 고르는 기능 구현(중복은 2개까지 허용)

--- src/main/kotlin/menu/domain/CoachManager.kt
+++ src/main/kotlin/menu/domain/CoachManager.kt
@@ -1,11 +1,25 @@
 package menu.domain
 
-class CoachManager(coachNames: List<String>,
-                   unavailableFoods: List<List<String>>) {
+class CoachManager(
+    coachNames: List<String>,
+    unavailableFoods: List<List<String>>
+) {
     private val coaches = mutableListOf<Coach>()
+    private val recommender = Recommender()
+    private val categories = mutableListOf<String>()  //  카테고리 저장
+
     init {
-        for (i in coachNames.indices){
+        for (i in coachNames.indices) {
             coaches.add(Coach(coachNames[i], unavailableFoods[i]))
         }
+    }
+
+    fun pickCategory() {
+        do {
+            val newCategory = recommender.pickCategory()
+            if (categories.count{ it == newCategory } < 3){
+                categories.add(newCategory)
+            }
+        } while (categories.size <= 5)
     }
 }
