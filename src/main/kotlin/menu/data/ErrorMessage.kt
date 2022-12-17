37b19171f004d820207935ecf685e13dc8982cd5
s9hn <tnghqn@naver.com>
2022-12-17 13:56:44 +0900

    [FEAT(ErrorMessage)] 에러메시지 상수화

---
+++ src/main/kotlin/menu/data/ErrorMessage.kt
@@ -0,0 +1,6 @@
+package menu.data
+
+const val ERROR_HEADER = "[ERROR] "
+const val ERROR_COACH_NAME = "코치의 이름은 최소 2글자, 최대 4글자입니다."
+const val ERROR_INPUT_EMPTY = "입력값이 비어있습니다."
+const val ERROR_RETRY = "다시 입력바랍니다."
