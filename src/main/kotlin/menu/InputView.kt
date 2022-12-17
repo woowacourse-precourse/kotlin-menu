c2bd5360bb42cb3953f4c48afadbe5a43ecafdf0
jjiiin <wjdwlin4051@gmail.com>
2022-12-17 16:38:52 +0900

    feat(InputView): 사용자에게서 입력값 받음

---
+++ src/main/kotlin/menu/InputView.kt
@@ -0,0 +1,15 @@
+package menu
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputView {
+
+
+    fun inputCoachName():String{
+        return Console.readLine()
+    }
+
+    fun inputCantEatMenu():String{
+        return Console.readLine()
+    }
+}
