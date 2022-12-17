548defcb5ed45ae9fe25c99da4938653afb808a2
vrexpert <vrexpert2302@gmail.com>
2022-12-17 16:53:21 +0900

    feat(controller): OutputView 작성

--- src/main/kotlin/menu/view/OutputView.kt
+++ src/main/kotlin/menu/view/OutputView.kt
@@ -1,11 +1,36 @@
 package menu.view
 
-class OutputView {
-    fun printMessage(){
+import menu.model.Coach
+import menu.values.Category
+import menu.values.GameMessage
 
+class OutputView {
+    fun printMessage(message:String){
+        println(message)
     }
 
-    fun printMenu(){
+    fun printMenu(weeklyCategory:List<Category>,coaches:List<Coach>){
+        println("메뉴 추천 결과입니다.")
+        println(GameMessage.WEEKLY_DAY)
+        printCategory(weeklyCategory)
+        repeat(coaches.size){index->
+            printOneDayMenu(coaches[index].name,coaches[index].weeklyMenus)
+        }
+        println("추천을 완료했습니다.")
+    }
 
+    private fun printCategory(weeklyCategory:List<Category>){
+        print("[ 카테고리 ")
+        repeat(weeklyCategory.size){index->
+            print(GameMessage.CATEGORY_PRINT.format(weeklyCategory[index].koreanName))
+        }
+        println("]")
+    }
+    private fun printOneDayMenu(name:String,weeklyMenus:List<String>){
+        print("[ $name ")
+        repeat(weeklyMenus.size) {index->
+            print(GameMessage.MENU_PRINT.format(weeklyMenus[index]))
+        }
+        println("]")
     }
 }
