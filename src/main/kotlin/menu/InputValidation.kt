636fba8b8a927357ef74ed14267718188024310a
choiwoosuk <wooseokchoi0508@gmail.com>
2022-12-17 13:42:50 +0900

    feat: 음식 입력값이 적절하지 않을 시 에러 발생 기능 구현

--- src/main/kotlin/menu/InputValidation.kt
+++ src/main/kotlin/menu/InputValidation.kt
@@ -13,10 +13,22 @@ class InputValidation {
         }) throw IllegalArgumentException()
     }
 
+    fun checkNumberOfImpossibleFood(input: String){
+        val foods = input.split(",")
+        if(foods.size> MAX_IMPOSSIBLE_FOOD_NUMBER ) throw IllegalArgumentException()
+    }
+
+    fun checkInvalidImpossibleFood(input: String,menu:List<String>){
+        if(!menu.contains(input)) throw IllegalArgumentException()
+    }
+
+
+
     companion object{
         const val MAX_COACH_NUMBER = 5
         const val MIN_COACH_NUMBER = 2
         const val MIN_COACH_NAME_LENGTH = 2
         const val MAX_COACH_NAME_LENGTH = 4
+        const val MAX_IMPOSSIBLE_FOOD_NUMBER = 2
     }
 }
