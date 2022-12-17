7691e087d4ad32857ac70561e08b878e42b0c5c9
JunJangE <wnswkd486@gmail.com>
2022-12-17 13:37:38 +0900

    feat: coachNamePrint 기능 추가

--- src/main/kotlin/menu/utils/Constant.kt
+++ src/main/kotlin/menu/utils/Constant.kt
@@ -2,4 +2,6 @@ package menu.utils
 
 object Constant {
     const val menuRecommendationStartMessage = "점심 메뉴 추천을 시작합니다."
+    const val coachNameMessage = "코치의 이름을 입력해 주세요. (, 로 구분)"
+
 }
--- src/main/kotlin/menu/view/OutputView.kt
+++ src/main/kotlin/menu/view/OutputView.kt
@@ -4,4 +4,5 @@ import menu.utils.Constant
 
 class OutputView {
     fun menuRecommendationStartPrint() = println(Constant.menuRecommendationStartMessage)
+    fun coachNamePrint() = println(Constant.coachNameMessage)
 }
