3cf1061e9310093434f53fc05d4e0f060ea0f865
lee989898 <wnsrud1352@gmail.com>
2022-12-17 13:37:58 +0900

    [feat](MenuCategory) 메뉴 카테고리(일식, 한식, 중식, 아시안, 양식) 데이터 클래스 구현

---
+++ src/main/kotlin/menu/MenuCategory.kt
@@ -0,0 +1,9 @@
+package menu
+
+data class MenuCategory(
+    val japanese: List<String>,
+    val korean: List<String>,
+    val chines: List<String>,
+    val asian: List<String>,
+    val western: List<String>
+)
