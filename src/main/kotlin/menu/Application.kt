816bb2069611dacc7201e5acee596004f141e004
brian <briandr@naver.com>
2022-12-17 17:02:31 +0900

    feat(Application): Controller를 이용해 전체 로직을 실행

--- src/main/kotlin/menu/Application.kt
+++ src/main/kotlin/menu/Application.kt
@@ -1,5 +1,15 @@
 package menu
 
+import menu.controller.Controller
+import menu.view.InputValidator
+import menu.view.InputView
+import menu.view.OutputView
+
 fun main() {
-    // TODO: 프로그램 구현
+    val controller = Controller(outputView = OutputView(), inputView = InputView(InputValidator()))
+
+    controller.start()
+    controller.inputNames()
+    controller.inputInedibleMenus()
+    controller.suggestMenus()
 }
