504b3fc16f39300519e8f87704254dc1169ba656
Boram <lboram20@naver.com>
2022-12-17 14:38:25 +0900

    feat<Application>: 메뉴를 추천받을 코치들이 못 먹는 메뉴를 각각 입력 받는다.

--- src/main/kotlin/menu/Application.kt
+++ src/main/kotlin/menu/Application.kt
@@ -3,6 +3,14 @@ package menu
 fun main() {
     val input = InputView()
 
+    val dislikeMenus = mutableListOf<List<String>>()
+
     println("점심 메뉴 추천을 시작합니다.")
     val coaches = input.getCoachNames()
+
+    for (coachIndex in coaches.indices) {
+        val coachName = coaches[coachIndex]
+        val dislike = input.getDislikeMenu(coachName)
+        dislikeMenus.add(dislike)
+    }
 }
