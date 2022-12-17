9bf9f3e92eb0ce1a99f93d08b384770473b5d732
choiwoosuk <wooseokchoi0508@gmail.com>
2022-12-17 17:12:39 +0900

    feat: 컨트롤러 추가

---
+++ src/main/kotlin/menu/Controller.kt
@@ -0,0 +1,46 @@
+package menu
+
+class Controller {
+
+    private val outputView = OutputView()
+    private val inputView = InputView()
+    private val launchMenuService = LaunchMenuService()
+    private lateinit var coach: Coach
+    private lateinit var categories: List<Category>
+
+    fun run(){
+        outputView.printServiceStart()
+        getCoachInformation()
+        outputView.printMenuResultInitialMessage()
+        outputView.printDayOfTheWeekMessage()
+        getCategories()
+        getCoachMenus()
+        outputView.printServiceEnd()
+    }
+
+    fun getCoachInformation(){
+        val coachNames = inputView.readCoaches()
+        val impossibleMenus = mutableListOf<List<String>>()
+        coachNames.forEach{coachName->
+            impossibleMenus.add(inputView.readImpossibleFood(coachName))
+        }
+        coach = Coach(coachNames,impossibleMenus)
+    }
+
+    fun getCategories(){
+        categories = launchMenuService.getCategories(CategoriesRandomGenerator())
+        outputView.printCategories(launchMenuService.categoryToString(categories))
+    }
+
+    fun getCoachMenus(){
+        for(index in 0 until coach.name.size){
+           getCoachMenu(index)
+        }
+    }
+
+    fun getCoachMenu(index:Int){
+        val menus = launchMenuService.getMenus(categories,coach.impossibleMenus[index],MenuRandomGenerator())
+        outputView.printMenus(coach.name[index],menus)
+    }
+
+}
