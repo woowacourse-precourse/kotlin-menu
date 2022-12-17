ec245863f3ed55b9a079013e9bb2f34bc12be003
inseonyun <yuninseon@naver.com>
2022-12-17 13:42:28 +0900

    feat: 에러 문구 클래스 생성

---
+++ src/main/kotlin/menu/message/Message.kt
@@ -0,0 +1,9 @@
+package menu.message
+
+class Message {
+    companion object {
+        const val isNotMoreThanTwo = "[ERROR] 입력된 코치가 두명 미만입니다."
+        const val isMoreThanSix = "[ERROR] 입력된 코치가 여섯명 이상입니다."
+        const val isNotAvailableName = "[ERROR] 입력된 코치 이름 중 최소 2글자, 최대 4글자를 지키지 못하는 값이 있습니다."
+    }
+}
