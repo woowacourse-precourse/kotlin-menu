e75f72d5bacec296f60341a7d7e8f936ecc3085c
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-12-17 17:02:11 +0900

    test[menu]: 이름의 길이에 대한 예외 테스트 코드 추가

--- src/test/kotlin/menu/ValidTest.kt
+++ src/test/kotlin/menu/ValidTest.kt
@@ -3,6 +3,8 @@ package menu
 import menu.util.Validator
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class ValidTest {
 
@@ -10,6 +12,14 @@ class ValidTest {
     fun `이름이 한글로 이루어지지 않은 경우`() {
         assertThrows<IllegalArgumentException> {
             Validator().checkNameValid("1하,")
+        }
+    }
+
+    @ValueSource(strings=["제임스롤롤", "가나다라마바사", ""])
+    @ParameterizedTest
+    fun `이름이 최소 2글자 최대 4글자가 아닌 경우`(input : String){
+        assertThrows<IllegalArgumentException> {
+            Validator().checkNameSizeValid(input)
         }
     }
 }
