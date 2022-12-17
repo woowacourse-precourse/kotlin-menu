529439cfcfa058b63ab892ebc15f7e5be395efac
dlgocks1 <cksgodl1@naver.com>
2022-12-17 15:50:04 +0900

    feat : 컨트롤러에서 요구하는 로직을 수행하기 위한 함수 추가
    * checkDiet : 식단 중복 체크
    * addDiet :식단 추가
    * toString() 함수 오버라이드

--- src/main/kotlin/menu/domain/model/Coach.kt
+++ src/main/kotlin/menu/domain/model/Coach.kt
@@ -1,9 +1,16 @@
 package menu.domain.model
 
+import menu.util.LEFT_BRACKET
+import menu.util.RIGHT_BRACKET
+import menu.util.SEPERATOR
+import java.lang.StringBuilder
+
 class Coach(
     private val name: String,
     private val inedible: MutableList<Food> = mutableListOf()
 ) {
+
+    private val diet = mutableListOf<Food>()
 
     init {
         require(name.length in 2..4) {
@@ -11,19 +18,49 @@ class Coach(
         }
     }
 
+    override fun toString(): String {
+        val stringBuilder = StringBuilder()
+        stringBuilder.append(LEFT_BRACKET)
+        stringBuilder.append(name)
+        stringBuilder.append(SEPERATOR)
+        for (i in diet.indices) {
+            stringBuilder.append(diet[i].getName())
+            if (i == diet.size - 1) continue
+            stringBuilder.append(SEPERATOR)
+        }
+        stringBuilder.append(RIGHT_BRACKET)
+        return stringBuilder.toString()
+    }
+
     fun getName() = name
 
+    fun equalName(checkingName: String): Boolean {
+        return name == checkingName
+    }
+
     fun addInedible(food: Food) {
+        indedibleValidateCheck(food)
+        inedible.add(food)
+    }
+
+    fun addDiet(food: Food) {
+        diet.add(food)
+    }
+
+    fun checkDiet(food: Food): Boolean {
+        if (diet.contains(food)) {
+            return true
+        }
+        return false
+    }
+
+    private fun indedibleValidateCheck(food: Food) {
         require(!inedible.contains(food)) {
             "해당 음식은 이미 입력됬습니다."
         }
         require(inedible.size <= 1) {
             "금식은 최대 2개까지 입니다."
         }
-        inedible.add(food)
     }
 
-    fun equalName(checkingName: String): Boolean {
-        return name == checkingName
-    }
 }
