54465d237b053359f6edb0324833340fda48d38d
EmilyCh0 <seoyeon3089@naver.com>
2022-12-17 16:35:14 +0900

    feat(Controller): run, 전체적인 프로세스

--- src/main/kotlin/menu/Controller.kt
+++ src/main/kotlin/menu/Controller.kt
@@ -5,7 +5,30 @@ class Controller(
     private val outputView: OutputView,
     private val inputValidator: InputValidator
 ) {
+    private val menu = Menu()
+    private lateinit var coaches: List<String>
+    private lateinit var lunchMenuRecommender: LunchMenuRecommender
+
     fun run() {
+        outputView.printStartMessage()
+        lunchMenuRecommender = initialize()
+        startRecommendation(lunchMenuRecommender)
+        outputView.printEndMessage()
+    }
+
+    private fun initialize(): LunchMenuRecommender {
+        TODO()
+    }
+
+    private fun startRecommendation(lunchMenuRecommender: LunchMenuRecommender) {
+        TODO()
+    }
+
+    private fun getCoachNames() {
+
+    }
+
+    private fun getDislikeMenus() {
 
     }
 }
