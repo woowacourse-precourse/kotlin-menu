ba43dfb14631a1da4a1d2588859c3559d678bc79
윤동현 <donghyunyoon81@gmail.com>
2022-12-20 10:00:20 +0900

    feat<Coach> : addRecommendedMenus 기능 구현
    해당 카테고리의 무작위 음식 하나를 recommendedMenus에 추가하는 기능

--- src/main/kotlin/menu/model/Coach.kt
+++ src/main/kotlin/menu/model/Coach.kt
@@ -1,7 +1,15 @@
 package menu.model
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Coach(private val name: String) {
 
+    private val recommendedMenus = mutableListOf<String>()
+
     fun getName() = name
+
+    fun addRecommendedMenus(category: Category,menu: Menu) {
+        recommendedMenus.add(Randoms.shuffle(menu.getCategoryMenu(category.getCategoryName()))[0])
+    }
 
 }
