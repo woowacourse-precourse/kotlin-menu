6a271af23d4202b4b0d5058f4fc07e85e83e7101
윤동현 <donghyunyoon81@gmail.com>
2022-12-20 11:57:01 +0900

    feat<Coach> : getExcludeMenus,addExcludeMenus 기능 구현
    코치가 제외할 음식들을 반환하고 추가하는 기능

--- src/main/kotlin/menu/model/Coach.kt
+++ src/main/kotlin/menu/model/Coach.kt
@@ -5,8 +5,17 @@ import camp.nextstep.edu.missionutils.Randoms
 class Coach(private val name: String) {
 
     private val recommendedMenus = mutableListOf<String>()
+    private val excludeMenus = mutableListOf<String>()
 
     fun getName() = name
+
+    fun getExcludeMenus() = excludeMenus
+
+    fun addExcludeMenus(menus: List<String>) {
+        menus.forEach { menu ->
+            excludeMenus.add(menu)
+        }
+    }
 
     fun getRecommendedMenus() = recommendedMenus
 
